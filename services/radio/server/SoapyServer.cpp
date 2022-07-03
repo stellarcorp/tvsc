b7e64d45eea3391559ef04d655bd84d34b276474
James Pack <jgrimm@gmail.com>
2022-07-03 08:25:23 -0400

    Begin taking ownership of the main() function for the radio service

--- services/radio/server/SoapyServer.cpp
+++ services/radio/server/SoapyServer.cpp
@@ -1,144 +1,97 @@
-// Copyright (c) 2015-2018 Josh Blum
-// SPDX-License-Identifier: BSL-1.0
-
 #include "SoapyServer.hpp"
-#include "SoapyRemoteDefs.hpp"
-#include "SoapyURLUtils.hpp"
-#include "SoapyInfoUtils.hpp"
-#include "SoapyRPCSocket.hpp"
-#include "SoapySSDPEndpoint.hpp"
-#include "SoapyMDNSEndpoint.hpp"
-#include <cstdlib>
-#include <cstddef>
-#include <iostream>
-#include <getopt.h>
-#include <csignal>
 
-/***********************************************************************
- * Print help message
- **********************************************************************/
-static int printHelp(void)
-{
-    std::cout << "Usage SoapySDRServer [options]" << std::endl;
-    std::cout << "  Options summary:" << std::endl;
-    std::cout << "    --help \t\t\t\t Print this help message" << std::endl;
-    std::cout << "    --bind \t\t\t\t Bind and serve forever" << std::endl;
-    std::cout << std::endl;
-    return EXIT_SUCCESS;
+#include <csignal>
+#include <cstddef>
+#include <cstdlib>
+
+#include "SoapyInfoUtils.hpp"
+#include "SoapyMDNSEndpoint.hpp"
+#include "SoapyRPCSocket.hpp"
+#include "SoapyRemoteDefs.hpp"
+#include "SoapySSDPEndpoint.hpp"
+#include "SoapyURLUtils.hpp"
+#include "gflags/gflags.h"
+#include "glog/logging.h"
+
+DEFINE_string(soapy_url, "0.0.0.0:55132",
+              "URL to bind the SoapyRemote service to. Can be an IPv6 or IPv4 address, as supported by network.");
+
+bool is_ipv6_supported() {
+  static bool isIPv6Supported{not SoapyRPCSocket(SoapyURL("tcp", "::", "0").toString()).null()};
+  return isIPv6Supported;
 }
 
 /***********************************************************************
  * Signal handler for Ctrl + C
  **********************************************************************/
 static sig_atomic_t serverDone = false;
-void sigIntHandler(const int)
-{
-    std::cout << "Caught Ctrl+C, shutting down the server..." << std::endl;
-    serverDone = true;
+void sigIntHandler(const int) {
+  LOG(INFO) << "Caught Ctrl+C, shutting down the server...";
+  serverDone = true;
 }
 
-/***********************************************************************
- * Launch the server
- **********************************************************************/
-static int runServer(void)
-{
-    SoapySocketSession sess;
-    const bool isIPv6Supported = not SoapyRPCSocket(SoapyURL("tcp", "::", "0").toString()).null();
-    const auto defaultBindNode = isIPv6Supported?"::":"0.0.0.0";
-    const int ipVerServices = isIPv6Supported?SOAPY_REMOTE_IPVER_UNSPEC:SOAPY_REMOTE_IPVER_INET;
+static int runServer(void) {
+  SoapySocketSession sess;
 
-    //extract url from user input or generate automatically
-    const bool optargHasURL = (optarg != NULL and not std::string(optarg).empty());
-    auto url = (optargHasURL)? SoapyURL(optarg) : SoapyURL("tcp", defaultBindNode, "");
+  auto url = SoapyURL(FLAGS_soapy_url);
 
-    //default url parameters when not specified
-    if (url.getScheme().empty()) url.setScheme("tcp");
-    if (url.getService().empty()) url.setService(SOAPY_REMOTE_DEFAULT_SERVICE);
+  // this UUID identifies the server process
+  const auto serverUUID = SoapyInfo::generateUUID1();
+  LOG(INFO) << "Server version: " << SoapyInfo::getServerVersion();
+  LOG(INFO) << "Server UUID: " << serverUUID;
 
-    //this UUID identifies the server process
-    const auto serverUUID = SoapyInfo::generateUUID1();
-    std::cout << "Server version: " << SoapyInfo::getServerVersion() << std::endl;
-    std::cout << "Server UUID: " << serverUUID << std::endl;
+  LOG(INFO) << "Launching the server... " << url.toString();
+  SoapyRPCSocket s;
+  if (s.bind(url.toString()) != 0) {
+    LOG(FATAL) << "Server socket bind FAIL: " << s.lastErrorMsg();
+    return EXIT_FAILURE;
+  }
+  LOG(INFO) << "Server bound to " << s.getsockname();
+  s.listen(SOAPY_REMOTE_LISTEN_BACKLOG);
+  auto serverListener = new SoapyServerListener(s, serverUUID);
 
-    std::cout << "Launching the server... " << url.toString() << std::endl;
-    SoapyRPCSocket s;
-    if (s.bind(url.toString()) != 0)
-    {
-        std::cerr << "Server socket bind FAIL: " << s.lastErrorMsg() << std::endl;
-        return EXIT_FAILURE;
+  const int ipVerServices = is_ipv6_supported() ? SOAPY_REMOTE_IPVER_UNSPEC : SOAPY_REMOTE_IPVER_INET;
+
+  LOG(INFO) << "Launching discovery server... ";
+  auto ssdpEndpoint = new SoapySSDPEndpoint();
+  ssdpEndpoint->registerService(serverUUID, url.getService(), ipVerServices);
+
+  LOG(INFO) << "Connecting to DNS-SD daemon... ";
+  auto dnssdPublish = new SoapyMDNSEndpoint();
+  dnssdPublish->printInfo();
+  dnssdPublish->registerService(serverUUID, url.getService(), ipVerServices);
+
+  LOG(INFO) << "Press Ctrl+C to stop the server";
+  signal(SIGINT, sigIntHandler);
+  bool exitFailure = false;
+  while (not serverDone and not exitFailure) {
+    serverListener->handleOnce();
+    if (not s.status()) {
+      LOG(ERROR) << "Server socket failure: " << s.lastErrorMsg();
+      exitFailure = true;
     }
-    std::cout << "Server bound to " << s.getsockname() << std::endl;
-    s.listen(SOAPY_REMOTE_LISTEN_BACKLOG);
-    auto serverListener = new SoapyServerListener(s, serverUUID);
-
-    std::cout << "Launching discovery server... " << std::endl;
-    auto ssdpEndpoint = new SoapySSDPEndpoint();
-    ssdpEndpoint->registerService(serverUUID, url.getService(), ipVerServices);
-
-    std::cout << "Connecting to DNS-SD daemon... " << std::endl;
-    auto dnssdPublish = new SoapyMDNSEndpoint();
-    dnssdPublish->printInfo();
-    dnssdPublish->registerService(serverUUID, url.getService(), ipVerServices);
-
-    std::cout << "Press Ctrl+C to stop the server" << std::endl;
-    signal(SIGINT, sigIntHandler);
-    bool exitFailure = false;
-    while (not serverDone and not exitFailure)
-    {
-        serverListener->handleOnce();
-        if (not s.status())
-        {
-            std::cerr << "Server socket failure: " << s.lastErrorMsg() << std::endl;
-            exitFailure = true;
-        }
-        if (not dnssdPublish->status())
-        {
-            std::cerr << "DNS-SD daemon disconnected..." << std::endl;
-            exitFailure = true;
-        }
+    if (not dnssdPublish->status()) {
+      LOG(ERROR) << "DNS-SD daemon disconnected...";
+      exitFailure = true;
     }
-    if (exitFailure) std::cerr << "Exiting prematurely..." << std::endl;
+  }
+  if (exitFailure) {
+    LOG(ERROR) << "Exiting prematurely...";
+  }
 
-    delete ssdpEndpoint;
-    delete dnssdPublish;
+  delete ssdpEndpoint;
+  delete dnssdPublish;
 
-    std::cout << "Shutdown client handler threads" << std::endl;
-    delete serverListener;
-    s.close();
+  LOG(INFO) << "Shutdown client handler threads";
+  delete serverListener;
+  s.close();
 
-    std::cout << "Cleanup complete, exiting" << std::endl;
-    return exitFailure?EXIT_FAILURE:EXIT_SUCCESS;
+  LOG(INFO) << "Cleanup complete, exiting";
+  return exitFailure ? EXIT_FAILURE : EXIT_SUCCESS;
 }
 
-/***********************************************************************
- * Parse and dispatch options
- **********************************************************************/
-int main(int argc, char *argv[])
-{
-    std::cout << "######################################################" << std::endl;
-    std::cout << "## Soapy Server -- Use any Soapy SDR remotely" << std::endl;
-    std::cout << "######################################################" << std::endl;
-    std::cout << std::endl;
-
-    /*******************************************************************
-     * parse command line options
-     ******************************************************************/
-    static struct option long_options[] = {
-        {"help", no_argument, 0, 'h'},
-        {"bind", optional_argument, 0, 'b'},
-        {0, 0, 0,  0}
-    };
-    int long_index = 0;
-    int option = 0;
-    while ((option = getopt_long_only(argc, argv, "", long_options, &long_index)) != -1)
-    {
-        switch (option)
-        {
-        case 'h': return printHelp();
-        case 'b': return runServer();
-        }
-    }
-
-    //unknown or unspecified options, do help...
-    return printHelp();
+int main(int argc, char *argv[]) {
+  google::InitGoogleLogging(argv[0]);
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+  runServer();
 }
