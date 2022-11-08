de9471eeaa4c90b5e1612923b463ae8de1611040
James Pack <jgrimm@gmail.com>
2022-11-08 12:10:06 -0500

    Implementation of std::array-like buffer type with bulk operations and optimizations for trivially copyable types

---
+++ buffer/buffer.h
@@ -0,0 +1,177 @@
+#pragma once
+
+#include <array>
+#include <cstring>
+#include <stdexcept>
+#include <string>
+
+namespace tvsc::buffer {
+
+/**
+ * std::array-like buffer type with bulk operations based on memcpy(3) for trivially copyable types.
+ *
+ * Buffer is a simple buffer type, similar to std::array, but with bulk read/write operations and optimizations for
+ * trivially copyable types.
+ *
+ * BufferT has two template overloads, one for trivially copyable element types, and one where the element type is not
+ * trivially copyable. The non-trivial element implementation uses loops to move data. The implement for elements that
+ * are trivially copyable is based on memcpy.
+ *
+ * The Buffer type makes the syntax of choosing the correct BufferT template overload easier. It is defined after the
+ * two template overloads.
+ */
+template <typename ElementT, size_t NUM_ELEMENTS, bool is_trivially_copyable>
+class BufferT final {
+ private:
+  static_assert(NUM_ELEMENTS > 0, "Number of elements in the buffer must be positive");
+
+  ElementT elements_[NUM_ELEMENTS];
+
+  void validate_index(size_t index) const {
+    if (index < 0 || index >= NUM_ELEMENTS) {
+      using std::to_string;
+      throw std::out_of_range("Invalid index " + to_string(index) + " (NUM_ELEMENTS: " + to_string(NUM_ELEMENTS));
+    }
+  }
+
+ public:
+  const ElementT& read(size_t index) const { return operator[](index); }
+
+  void read(size_t offset, size_t count, ElementT dest[]) const {
+    validate_index(offset);
+    validate_index(offset + count - 1);
+    for (size_t i = 0; i < count; ++i) {
+      dest[i] = operator[](i + offset);
+    }
+  }
+
+  template <size_t ARRAY_SIZE>
+  void read(size_t offset, size_t count, std::array<ElementT, ARRAY_SIZE>& dest) const {
+    validate_index(offset);
+    validate_index(offset + count - 1);
+    if (ARRAY_SIZE < count) {
+      using std::to_string;
+      throw std::overflow_error("dest has insufficient space (" + to_string(count) + " vs " + to_string(ARRAY_SIZE) +
+                                ")");
+    }
+    for (size_t i = 0; i < count; ++i) {
+      dest[i] = operator[](i + offset);
+    }
+  }
+
+  void write(size_t index, const ElementT& element) { operator[](index) = element; }
+
+  void write(size_t index, ElementT&& element) { operator[](index) = std::move(element); }
+
+  void write(size_t offset, size_t count, const ElementT src[]) {
+    validate_index(offset);
+    validate_index(offset + count - 1);
+    for (size_t i = 0; i < count; ++i) {
+      operator[](i + offset) = src[i];
+    }
+  }
+
+  template <size_t ARRAY_SIZE>
+  void write(size_t offset, size_t count, const std::array<ElementT, ARRAY_SIZE>& src) {
+    validate_index(offset);
+    validate_index(offset + count - 1);
+    if (ARRAY_SIZE < count) {
+      using std::to_string;
+      throw std::overflow_error("src has insufficient space (" + to_string(count) + " vs " + to_string(ARRAY_SIZE) +
+                                ")");
+    }
+    for (size_t i = 0; i < count; ++i) {
+      operator[](i + offset) = src[i];
+    }
+  }
+
+  const ElementT& operator[](size_t index) const {
+    validate_index(index);
+    return elements_[index];
+  }
+
+  ElementT& operator[](size_t index) {
+    validate_index(index);
+    return elements_[index];
+  }
+
+  constexpr ElementT* data() noexcept { return elements_; }
+  constexpr const ElementT* data() const noexcept { return elements_; }
+};
+
+// BufferT implementation for trivially copyable ElementT types.
+template <typename ElementT, size_t NUM_ELEMENTS>
+class BufferT<ElementT, NUM_ELEMENTS, true> final {
+ private:
+  static_assert(NUM_ELEMENTS > 0, "Number of elements in the buffer must be positive");
+
+  ElementT elements_[NUM_ELEMENTS];
+
+  void validate_index(size_t index) const {
+    if (index < 0 || index >= NUM_ELEMENTS) {
+      using std::to_string;
+      throw std::out_of_range("Invalid index " + to_string(index) + " (NUM_ELEMENTS: " + to_string(NUM_ELEMENTS));
+    }
+  }
+
+ public:
+  const ElementT& read(size_t index) const { return operator[](index); }
+
+  void read(size_t offset, size_t count, ElementT dest[]) const {
+    validate_index(offset);
+    validate_index(offset + count - 1);
+    std::memcpy(dest, elements_ + offset, count * sizeof(ElementT));
+  }
+
+  template <size_t ARRAY_SIZE>
+  void read(size_t offset, size_t count, std::array<ElementT, ARRAY_SIZE>& dest) const {
+    validate_index(offset);
+    validate_index(offset + count - 1);
+    if (ARRAY_SIZE < count) {
+      using std::to_string;
+      throw std::overflow_error("dest has insufficient space (" + to_string(count) + " vs " + to_string(ARRAY_SIZE) +
+                                ")");
+    }
+    std::memcpy(dest.data(), elements_ + offset, count * sizeof(ElementT));
+  }
+
+  void write(size_t index, const ElementT& element) { operator[](index) = element; }
+
+  void write(size_t index, ElementT&& element) { operator[](index) = std::move(element); }
+
+  void write(size_t offset, size_t count, const ElementT src[]) {
+    validate_index(offset);
+    validate_index(offset + count - 1);
+    std::memcpy(elements_ + offset, src, count * sizeof(ElementT));
+  }
+
+  template <size_t ARRAY_SIZE>
+  void write(size_t offset, size_t count, const std::array<ElementT, ARRAY_SIZE>& src) {
+    validate_index(offset);
+    validate_index(offset + count - 1);
+    if (ARRAY_SIZE < count) {
+      using std::to_string;
+      throw std::overflow_error("src has insufficient space (" + to_string(count) + " vs " + to_string(ARRAY_SIZE) +
+                                ")");
+    }
+    std::memcpy(elements_ + offset, src.data(), count * sizeof(ElementT));
+  }
+
+  const ElementT& operator[](size_t index) const {
+    validate_index(index);
+    return elements_[index];
+  }
+
+  ElementT& operator[](size_t index) {
+    validate_index(index);
+    return elements_[index];
+  }
+
+  constexpr ElementT* data() noexcept { return elements_; }
+  constexpr const ElementT* data() const noexcept { return elements_; }
+};
+
+template <typename ElementT, size_t NUM_ELEMENTS>
+using Buffer = BufferT<ElementT, NUM_ELEMENTS, std::is_trivially_copyable<ElementT>::value>;
+
+}  // namespace tvsc::buffer
---
+++ buffer/buffer_test.cc
@@ -0,0 +1,489 @@
+#include "buffer/buffer.h"
+
+#include <array>
+
+#include "gtest/gtest.h"
+
+namespace tvsc::buffer {
+
+// Uncomment to verify compilation error due to static_assert violation.
+// TEST(BufferTest, CantCreateBuffersWithZeroElements) { Buffer<int, 0> buffer{}; }
+
+TEST(BufferTest, CanCreateBuffer) {
+  Buffer<int, 128> buffer{};
+  buffer[0] = 1;
+  buffer[127] = 128;
+  EXPECT_EQ(1, buffer[0]);
+  EXPECT_EQ(128, buffer[127]);
+}
+
+TEST(BufferTest, CanCreateTinyBuffer) {
+  Buffer<int, 1> buffer{};
+  buffer[0] = 1;
+  EXPECT_EQ(1, buffer[0]);
+}
+
+TEST(BufferTest, CanCreateLargeBuffer) {
+  constexpr size_t SIZE{1'000'000};
+  Buffer<int, SIZE> buffer{};
+  for (size_t i = 0; i < SIZE; ++i) {
+    buffer[i] = i;
+  }
+  for (size_t i = 0; i < SIZE; ++i) {
+    EXPECT_EQ(i, buffer[i]);
+  }
+}
+
+TEST(BufferTest, CanBulkReadViaCArray) {
+  constexpr size_t SIZE{1'024};
+  int other[SIZE];
+  Buffer<int, SIZE> buffer{};
+  for (size_t i = 0; i < SIZE; ++i) {
+    buffer[i] = i;
+  }
+  buffer.read(0, SIZE, other);
+  for (size_t i = 0; i < SIZE; ++i) {
+    EXPECT_EQ(i, other[i]);
+  }
+}
+
+TEST(BufferTest, CanBulkReadViaCArrayWithOffset) {
+  constexpr size_t SIZE{1'024};
+  constexpr size_t OFFSET{SIZE / 2};
+  int other[SIZE - OFFSET];
+  Buffer<int, SIZE> buffer{};
+  for (size_t i = 0; i < SIZE; ++i) {
+    buffer[i] = i;
+  }
+  buffer.read(OFFSET, SIZE - OFFSET, other);
+  for (size_t i = 0; i < SIZE - OFFSET; ++i) {
+    EXPECT_EQ(i + OFFSET, other[i]);
+  }
+}
+
+TEST(BufferTest, CanBulkReadViaStdArray) {
+  constexpr size_t SIZE{1'024};
+  std::array<int, SIZE> other{};
+  Buffer<int, SIZE> buffer{};
+  for (size_t i = 0; i < SIZE; ++i) {
+    buffer[i] = i;
+  }
+  buffer.read(0, SIZE, other);
+  for (size_t i = 0; i < SIZE; ++i) {
+    EXPECT_EQ(i, other[i]);
+  }
+}
+
+TEST(BufferTest, CanBulkReadViaStdArrayWithOffset) {
+  constexpr size_t SIZE{1'024};
+  constexpr size_t OFFSET{SIZE / 2};
+  std::array<int, SIZE - OFFSET> other{};
+  Buffer<int, SIZE> buffer{};
+  for (size_t i = 0; i < SIZE; ++i) {
+    buffer[i] = i;
+  }
+  buffer.read(OFFSET, SIZE - OFFSET, other);
+  for (size_t i = 0; i < SIZE - OFFSET; ++i) {
+    EXPECT_EQ(i + OFFSET, other[i]);
+  }
+}
+
+TEST(BufferTest, CanBulkWriteViaCArray) {
+  constexpr size_t SIZE{1'024};
+  int other[SIZE];
+  Buffer<int, SIZE> buffer{};
+  for (size_t i = 0; i < SIZE; ++i) {
+    other[i] = i;
+  }
+  buffer.write(0, SIZE, other);
+  for (size_t i = 0; i < SIZE; ++i) {
+    EXPECT_EQ(i, buffer[i]);
+  }
+}
+
+TEST(BufferTest, CanBulkWriteViaCArrayWithOffset) {
+  constexpr size_t SIZE{1'024};
+  constexpr size_t OFFSET{SIZE / 2};
+  int other[SIZE - OFFSET];
+  Buffer<int, SIZE> buffer{};
+  for (size_t i = 0; i < SIZE - OFFSET; ++i) {
+    other[i] = i;
+  }
+  buffer.write(OFFSET, SIZE - OFFSET, other);
+  for (size_t i = 0; i < SIZE; ++i) {
+    if (i < OFFSET) {
+      EXPECT_EQ(0, buffer[i]);
+    } else {
+      EXPECT_EQ(i - OFFSET, buffer[i]);
+    }
+  }
+}
+
+TEST(BufferTest, CanBulkWriteViaStdArray) {
+  constexpr size_t SIZE{1'024};
+  std::array<int, SIZE> other{};
+  Buffer<int, SIZE> buffer{};
+  for (size_t i = 0; i < SIZE; ++i) {
+    other[i] = i;
+  }
+  buffer.write(0, SIZE, other);
+  for (size_t i = 0; i < SIZE; ++i) {
+    EXPECT_EQ(i, buffer[i]);
+  }
+}
+
+TEST(BufferTest, CanBulkWriteViaStdArrayWithOffset) {
+  constexpr size_t SIZE{1'024};
+  constexpr size_t OFFSET{SIZE / 2};
+  std::array<int, SIZE - OFFSET> other{};
+  Buffer<int, SIZE> buffer{};
+  for (size_t i = 0; i < SIZE - OFFSET; ++i) {
+    other[i] = i;
+  }
+  buffer.write(OFFSET, SIZE - OFFSET, other);
+  for (size_t i = 0; i < SIZE; ++i) {
+    if (i < OFFSET) {
+      EXPECT_EQ(0, buffer[i]);
+    } else {
+      EXPECT_EQ(i - OFFSET, buffer[i]);
+    }
+  }
+}
+
+class TriviallyCopyableType final {
+ private:
+  int value_{};
+
+ public:
+  TriviallyCopyableType& operator=(const TriviallyCopyableType& rhs) = default;
+
+  TriviallyCopyableType& operator=(int value) {
+    value_ = value;
+    return *this;
+  }
+
+  int value() const { return value_; }
+
+  bool operator==(const TriviallyCopyableType& rhs) const { return value_ == rhs.value_; }
+};
+
+bool operator==(int lhs, const TriviallyCopyableType& rhs) { return lhs == rhs.value(); }
+bool operator==(const TriviallyCopyableType& lhs, int rhs) { return lhs.value() == rhs; }
+
+TEST(TriviallyCopyableBufferTest, TriviallyCopyableIsTriviallyCopyable) {
+  ASSERT_TRUE(std::is_trivially_copyable<TriviallyCopyableType>::value);
+}
+
+TEST(TriviallyCopyableBufferTest, CanCreateBuffer) {
+  Buffer<TriviallyCopyableType, 128> buffer{};
+  buffer[0] = 1;
+  buffer[127] = 128;
+  EXPECT_EQ(1, buffer[0]);
+  EXPECT_EQ(128, buffer[127]);
+}
+
+TEST(TriviallyCopyableBufferTest, CanCreateTinyBuffer) {
+  Buffer<TriviallyCopyableType, 1> buffer{};
+  buffer[0] = 1;
+  EXPECT_EQ(1, buffer[0]);
+}
+
+TEST(TriviallyCopyableBufferTest, CanCreateLargeBuffer) {
+  constexpr size_t SIZE{1'000'000};
+  Buffer<TriviallyCopyableType, SIZE> buffer{};
+  for (size_t i = 0; i < SIZE; ++i) {
+    buffer[i] = i;
+  }
+  for (size_t i = 0; i < SIZE; ++i) {
+    EXPECT_EQ(i, buffer[i]);
+  }
+}
+
+TEST(TriviallyCopyableBufferTest, CanBulkReadViaCArray) {
+  constexpr size_t SIZE{1'024};
+  TriviallyCopyableType other[SIZE];
+  Buffer<TriviallyCopyableType, SIZE> buffer{};
+  for (size_t i = 0; i < SIZE; ++i) {
+    buffer[i] = i;
+  }
+  buffer.read(0, SIZE, other);
+  for (size_t i = 0; i < SIZE; ++i) {
+    EXPECT_EQ(i, other[i]);
+  }
+}
+
+TEST(TriviallyCopyableBufferTest, CanBulkReadViaCArrayWithOffset) {
+  constexpr size_t SIZE{1'024};
+  constexpr size_t OFFSET{SIZE / 2};
+  TriviallyCopyableType other[SIZE - OFFSET];
+  Buffer<TriviallyCopyableType, SIZE> buffer{};
+  for (size_t i = 0; i < SIZE; ++i) {
+    buffer[i] = i;
+  }
+  buffer.read(OFFSET, SIZE - OFFSET, other);
+  for (size_t i = 0; i < SIZE - OFFSET; ++i) {
+    EXPECT_EQ(i + OFFSET, other[i]);
+  }
+}
+
+TEST(TriviallyCopyableBufferTest, CanBulkReadViaStdArray) {
+  constexpr size_t SIZE{1'024};
+  std::array<TriviallyCopyableType, SIZE> other{};
+  Buffer<TriviallyCopyableType, SIZE> buffer{};
+  for (size_t i = 0; i < SIZE; ++i) {
+    buffer[i] = i;
+  }
+  buffer.read(0, SIZE, other);
+  for (size_t i = 0; i < SIZE; ++i) {
+    EXPECT_EQ(i, other[i]);
+  }
+}
+
+TEST(TriviallyCopyableBufferTest, CanBulkReadViaStdArrayWithOffset) {
+  constexpr size_t SIZE{1'024};
+  constexpr size_t OFFSET{SIZE / 2};
+  std::array<TriviallyCopyableType, SIZE - OFFSET> other{};
+  Buffer<TriviallyCopyableType, SIZE> buffer{};
+  for (size_t i = 0; i < SIZE; ++i) {
+    buffer[i] = i;
+  }
+  buffer.read(OFFSET, SIZE - OFFSET, other);
+  for (size_t i = 0; i < SIZE - OFFSET; ++i) {
+    EXPECT_EQ(i + OFFSET, other[i]);
+  }
+}
+
+TEST(TriviallyCopyableBufferTest, CanBulkWriteViaCArray) {
+  constexpr size_t SIZE{1'024};
+  TriviallyCopyableType other[SIZE];
+  Buffer<TriviallyCopyableType, SIZE> buffer{};
+  for (size_t i = 0; i < SIZE; ++i) {
+    other[i] = i;
+  }
+  buffer.write(0, SIZE, other);
+  for (size_t i = 0; i < SIZE; ++i) {
+    EXPECT_EQ(i, buffer[i]);
+  }
+}
+
+TEST(TriviallyCopyableBufferTest, CanBulkWriteViaCArrayWithOffset) {
+  constexpr size_t SIZE{1'024};
+  constexpr size_t OFFSET{SIZE / 2};
+  TriviallyCopyableType other[SIZE - OFFSET];
+  Buffer<TriviallyCopyableType, SIZE> buffer{};
+  for (size_t i = 0; i < SIZE - OFFSET; ++i) {
+    other[i] = i;
+  }
+  buffer.write(OFFSET, SIZE - OFFSET, other);
+  for (size_t i = 0; i < SIZE; ++i) {
+    if (i < OFFSET) {
+      EXPECT_EQ(0, buffer[i]);
+    } else {
+      EXPECT_EQ(i - OFFSET, buffer[i]);
+    }
+  }
+}
+
+TEST(TriviallyCopyableBufferTest, CanBulkWriteViaStdArray) {
+  constexpr size_t SIZE{1'024};
+  std::array<TriviallyCopyableType, SIZE> other{};
+  Buffer<TriviallyCopyableType, SIZE> buffer{};
+  for (size_t i = 0; i < SIZE; ++i) {
+    other[i] = i;
+  }
+  buffer.write(0, SIZE, other);
+  for (size_t i = 0; i < SIZE; ++i) {
+    EXPECT_EQ(i, buffer[i]);
+  }
+}
+
+TEST(TriviallyCopyableBufferTest, CanBulkWriteViaStdArrayWithOffset) {
+  constexpr size_t SIZE{1'024};
+  constexpr size_t OFFSET{SIZE / 2};
+  std::array<TriviallyCopyableType, SIZE - OFFSET> other{};
+  Buffer<TriviallyCopyableType, SIZE> buffer{};
+  for (size_t i = 0; i < SIZE - OFFSET; ++i) {
+    other[i] = i;
+  }
+  buffer.write(OFFSET, SIZE - OFFSET, other);
+  for (size_t i = 0; i < SIZE; ++i) {
+    if (i < OFFSET) {
+      EXPECT_EQ(0, buffer[i]);
+    } else {
+      EXPECT_EQ(i - OFFSET, buffer[i]);
+    }
+  }
+}
+
+class NontrivialType final {
+ private:
+  int value_{};
+
+ public:
+  NontrivialType() = default;
+  NontrivialType(int value) : value_(value) {}
+
+  // Adding this copy constructor makes this type non-trivially copyable.
+  NontrivialType(const NontrivialType& rhs) : value_(rhs.value_) {}
+
+  NontrivialType& operator=(const NontrivialType& rhs) = default;
+
+  NontrivialType& operator=(int value) {
+    value_ = value;
+    return *this;
+  }
+
+  int value() const { return value_; }
+
+  bool operator==(const NontrivialType& rhs) const { return value_ == rhs.value_; }
+};
+
+bool operator==(int lhs, const NontrivialType& rhs) { return lhs == rhs.value(); }
+bool operator==(const NontrivialType& lhs, int rhs) { return lhs.value() == rhs; }
+
+TEST(NontrivialTypeBufferTest, TriviallyCopyableIsTriviallyCopyable) {
+  ASSERT_FALSE(std::is_trivially_copyable<NontrivialType>::value);
+}
+
+TEST(NontrivialTypeBufferTest, CanCreateBuffer) {
+  Buffer<NontrivialType, 128> buffer{};
+  buffer[0] = 1;
+  buffer[127] = 128;
+  EXPECT_EQ(1, buffer[0]);
+  EXPECT_EQ(128, buffer[127]);
+}
+
+TEST(NontrivialTypeBufferTest, CanCreateTinyBuffer) {
+  Buffer<NontrivialType, 1> buffer{};
+  buffer[0] = 1;
+  EXPECT_EQ(1, buffer[0]);
+}
+
+TEST(NontrivialTypeBufferTest, CanCreateLargeBuffer) {
+  constexpr size_t SIZE{1'000'000};
+  Buffer<NontrivialType, SIZE> buffer{};
+  for (size_t i = 0; i < SIZE; ++i) {
+    buffer[i] = i;
+  }
+  for (size_t i = 0; i < SIZE; ++i) {
+    EXPECT_EQ(i, buffer[i]);
+  }
+}
+
+TEST(NontrivialTypeBufferTest, CanBulkReadViaCArray) {
+  constexpr size_t SIZE{1'024};
+  NontrivialType other[SIZE];
+  Buffer<NontrivialType, SIZE> buffer{};
+  for (size_t i = 0; i < SIZE; ++i) {
+    buffer[i] = i;
+  }
+  buffer.read(0, SIZE, other);
+  for (size_t i = 0; i < SIZE; ++i) {
+    EXPECT_EQ(i, other[i]);
+  }
+}
+
+TEST(NontrivialTypeBufferTest, CanBulkReadViaCArrayWithOffset) {
+  constexpr size_t SIZE{1'024};
+  constexpr size_t OFFSET{SIZE / 2};
+  NontrivialType other[SIZE - OFFSET];
+  Buffer<NontrivialType, SIZE> buffer{};
+  for (size_t i = 0; i < SIZE; ++i) {
+    buffer[i] = i;
+  }
+  buffer.read(OFFSET, SIZE - OFFSET, other);
+  for (size_t i = 0; i < SIZE - OFFSET; ++i) {
+    EXPECT_EQ(i + OFFSET, other[i]);
+  }
+}
+
+TEST(NontrivialTypeBufferTest, CanBulkReadViaStdArray) {
+  constexpr size_t SIZE{1'024};
+  std::array<NontrivialType, SIZE> other{};
+  Buffer<NontrivialType, SIZE> buffer{};
+  for (size_t i = 0; i < SIZE; ++i) {
+    buffer[i] = i;
+  }
+  buffer.read(0, SIZE, other);
+  for (size_t i = 0; i < SIZE; ++i) {
+    EXPECT_EQ(i, other[i]);
+  }
+}
+
+TEST(NontrivialTypeBufferTest, CanBulkReadViaStdArrayWithOffset) {
+  constexpr size_t SIZE{1'024};
+  constexpr size_t OFFSET{SIZE / 2};
+  std::array<NontrivialType, SIZE - OFFSET> other{};
+  Buffer<NontrivialType, SIZE> buffer{};
+  for (size_t i = 0; i < SIZE; ++i) {
+    buffer[i] = i;
+  }
+  buffer.read(OFFSET, SIZE - OFFSET, other);
+  for (size_t i = 0; i < SIZE - OFFSET; ++i) {
+    EXPECT_EQ(i + OFFSET, other[i]);
+  }
+}
+
+TEST(NontrivialTypeBufferTest, CanBulkWriteViaCArray) {
+  constexpr size_t SIZE{1'024};
+  NontrivialType other[SIZE];
+  Buffer<NontrivialType, SIZE> buffer{};
+  for (size_t i = 0; i < SIZE; ++i) {
+    other[i] = i;
+  }
+  buffer.write(0, SIZE, other);
+  for (size_t i = 0; i < SIZE; ++i) {
+    EXPECT_EQ(i, buffer[i]);
+  }
+}
+
+TEST(NontrivialTypeBufferTest, CanBulkWriteViaCArrayWithOffset) {
+  constexpr size_t SIZE{1'024};
+  constexpr size_t OFFSET{SIZE / 2};
+  NontrivialType other[SIZE - OFFSET];
+  Buffer<NontrivialType, SIZE> buffer{};
+  for (size_t i = 0; i < SIZE - OFFSET; ++i) {
+    other[i] = i;
+  }
+  buffer.write(OFFSET, SIZE - OFFSET, other);
+  for (size_t i = 0; i < SIZE; ++i) {
+    if (i < OFFSET) {
+      EXPECT_EQ(0, buffer[i]);
+    } else {
+      EXPECT_EQ(i - OFFSET, buffer[i]);
+    }
+  }
+}
+
+TEST(NontrivialTypeBufferTest, CanBulkWriteViaStdArray) {
+  constexpr size_t SIZE{1'024};
+  std::array<NontrivialType, SIZE> other{};
+  Buffer<NontrivialType, SIZE> buffer{};
+  for (size_t i = 0; i < SIZE; ++i) {
+    other[i] = i;
+  }
+  buffer.write(0, SIZE, other);
+  for (size_t i = 0; i < SIZE; ++i) {
+    EXPECT_EQ(i, buffer[i]);
+  }
+}
+
+TEST(NontrivialTypeBufferTest, CanBulkWriteViaStdArrayWithOffset) {
+  constexpr size_t SIZE{1'024};
+  constexpr size_t OFFSET{SIZE / 2};
+  std::array<NontrivialType, SIZE - OFFSET> other{};
+  Buffer<NontrivialType, SIZE> buffer{};
+  for (size_t i = 0; i < SIZE - OFFSET; ++i) {
+    other[i] = i;
+  }
+  buffer.write(OFFSET, SIZE - OFFSET, other);
+  for (size_t i = 0; i < SIZE; ++i) {
+    if (i < OFFSET) {
+      EXPECT_EQ(0, buffer[i]);
+    } else {
+      EXPECT_EQ(i - OFFSET, buffer[i]);
+    }
+  }
+}
+
+}  // namespace tvsc::buffer
