6bd3b6c646362bd529eaf40938a6006021f68dc2
James Pack <jgrimm@gmail.com>
2022-11-13 09:21:07 -0500

    Remove soapy_remote_test. It's a failing test that doesn't do much anyway.

--- third_party/soapy_remote/soapy_remote_test.cc
+++
@@ -1,7 +0,0 @@
-#include "gtest/gtest.h"
-#include "SoapyInfoUtils.hpp"
-
-// Trivial test to verify compilation and linking.
-TEST(SoapyRemote, CanGetVersionString) {
-  EXPECT_FALSE(SoapyInfo::getServerVersion().empty());
-}
