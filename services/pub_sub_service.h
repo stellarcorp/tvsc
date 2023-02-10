20934a22ed4a83573db33a3a81ed0bd71d26396f
James Pack <jgrimm@gmail.com>
2023-02-10 15:52:55 -0500

    Redesign of PubSubService.

--- services/pub_sub_service.h
+++ services/pub_sub_service.h
@@ -1,13 +1,17 @@
 #pragma once
 
+#include <chrono>
+#include <condition_variable>
 #include <functional>
 #include <memory>
+#include <mutex>
 #include <string>
 #include <string_view>
 
 #include "glog/logging.h"
+#include "grpcpp/support/async_stream.h"
+#include "grpcpp/support/client_callback.h"
 #include "grpcpp/support/status.h"
-#include "grpcpp/support/sync_stream.h"
 
 namespace tvsc::services {
 
@@ -47,23 +51,35 @@ class Topic {
 };
 
 template <typename RequestT, typename ResponseT>
-class PubSubService final {
+class PubSubService final : grpc::ClientReadReactor<ResponseT> {
  public:
-  using RpcMethodCallerT = std::function<std::unique_ptr<grpc::ClientReaderInterface<ResponseT>>(
-      grpc::ClientContext* context, const RequestT& request)>;
+  using RpcMethodCallerT = std::function<void(grpc::ClientContext* context, const RequestT* request,
+                                              grpc::ClientReadReactor<ResponseT>* reactor)>;
 
  private:
   Topic<ResponseT>* web_socket_topic_;
-  std::unique_ptr<grpc::ClientReaderInterface<ResponseT>> grpc_reader_{};
+  // std::unique_ptr<grpc::ClientAsyncReaderInterface<ResponseT>> grpc_reader_{};
 
   RpcMethodCallerT call_rpc_fn_;
 
   grpc::ClientContext context_{};
   RequestT request_{};
 
+  ResponseT response_{};
   grpc::Status status_{};
 
+  mutable std::mutex mu_{};
+  mutable std::condition_variable cv_;
+
+  bool is_running_{false};
+
   void finalize(const grpc::Status& status) { status_ = status; }
+
+  /**
+   * Helper method to build a tag that can be used to tag entries in the gRPC completion queue.
+   * Note that only the address of the tag is significant, not its value.
+   */
+  // void* tag() { return reinterpret_cast<void*>(this); }
 
  public:
   PubSubService(Topic<ResponseT>& topic, RpcMethodCallerT call_rpc_fn)
@@ -72,24 +88,65 @@ class PubSubService final {
   PubSubService(Topic<ResponseT>& topic, RpcMethodCallerT call_rpc_fn, const RequestT& request)
       : web_socket_topic_(&topic), call_rpc_fn_(std::move(call_rpc_fn)), request_(request) {}
 
-  void loop() {
-    using namespace std::literals::chrono_literals;
+  template <typename Clock = std::chrono::system_clock>
 
-    grpc_reader_ = call_rpc_fn_(&context_, request_);
+  void start() {
+    DLOG(INFO) << "PubSubService::start()";
+    std::unique_lock<std::mutex> l(mu_);
+    if (!is_running_) {
+      LOG(INFO) << "PubSubService::start() -- initiating RPC";
+      call_rpc_fn_(&context_, &request_, this);
 
-    ResponseT response{};
-    while (grpc_reader_->Read(&response)) {
-      web_socket_topic_->publish(response);
+      is_running_ = true;
+
+      // Immediately request a read. These calls do not block.
+      this->StartRead(&response_);
+      this->StartCall();
     }
-    status_ = grpc_reader_->Finish();
+  }
+
+  void OnReadDone(bool ok) override {
+    DLOG(INFO) << "PubSubService::OnReadDone()";
+    std::unique_lock<std::mutex> l(mu_);
+    if (ok) {
+      // Publish the response.
+      web_socket_topic_->publish(response_);
+
+      // Initiate a new read.
+      this->StartRead(&response_);
+    }
+  }
+
+  void OnDone(const grpc::Status& s) override {
+    DLOG(INFO) << "PubSubService::OnDone()";
+    std::unique_lock<std::mutex> l(mu_);
+    status_ = s;
+    is_running_ = false;
+    cv_.notify_one();
   }
 
   void stop() {
+    DLOG(INFO) << "PubSubService::stop()";
+    std::unique_lock<std::mutex> l(mu_);
     context_.TryCancel();
-    grpc_reader_.reset();
   }
 
-  const grpc::Status& status() const { return status_; }
+  const grpc::Status& await() const {
+    DLOG(INFO) << "PubSubService::await()";
+    std::unique_lock<std::mutex> l(mu_);
+    cv_.wait(l, [this] { return !is_running_; });
+    return status_;
+  }
+
+  const grpc::Status& status() const {
+    std::unique_lock<std::mutex> l(mu_);
+    return status_;
+  }
+
+  bool is_running() const {
+    std::unique_lock<std::mutex> l(mu_);
+    return is_running_;
+  }
 };
 
 }  // namespace tvsc::services
--- services/pub_sub_service_test.cc
+++ services/pub_sub_service_test.cc
@@ -1,21 +1,30 @@
 #include "services/pub_sub_service.h"
 
+#include <chrono>
 #include <memory>
 #include <string>
 #include <string_view>
+#include <thread>
 
-#include "App.h"
 #include "glog/logging.h"
 #include "gmock/gmock.h"
 #include "google/protobuf/util/message_differencer.h"
-#include "services/test_mock.grpc.pb.h"
-#include "services/test_response_reader.h"
+#include "grpcpp/create_channel.h"
+#include "grpcpp/server_builder.h"
+#include "services/test.grpc.pb.h"
 
 namespace tvsc::services {
 
 using ::testing::_;
 using ::testing::ContainerEq;
+using ::testing::DoAll;
+using ::testing::IgnoreResult;
+using ::testing::Invoke;
 using ::testing::Return;
+using ::testing::SaveArg;
+using ::testing::WithArgs;
+
+using namespace std::literals::chrono_literals;
 
 constexpr char TOPIC_NAME[] = "topic";
 
@@ -49,53 +58,84 @@ class TestTopic final : public Topic<MessageT> {
   }
 };
 
-void add_test_message(TestReader<TestResponse>* backend, std::string message) {
-  TestResponse response{};
-  response.set_message(std::move(message));
-  backend->add_response(response);
-}
+class TestServiceImpl : public TestService::Service {
+ public:
+  std::vector<TestResponse> canned_responses{};
 
-TEST(PubSubServiceTest, CanPublishRpcResponseOverTopic) {
-  constexpr char MESSAGE[]{"message"};
+  grpc::Status server_stream(grpc::ServerContext* /*context*/, const TestRequest* /*request*/,
+                             grpc::ServerWriter<TestResponse>* writer) override {
+    for (const auto& response : canned_responses) {
+      DLOG(INFO) << "Server::writing response: " << response.DebugString();
+      std::this_thread::sleep_for(10ms);
+      writer->Write(response);
+    }
+    return grpc::Status::OK;
+  }
+};
 
-  MockTestServiceStub stub;
+class PubSubServiceTest : public ::testing::Test {
+ public:
+  std::ostringstream server_address{};
+  std::unique_ptr<grpc::Server> server{};
 
-  {
-    std::unique_ptr<TestReader<TestResponse>> backend{std::make_unique<TestReader<TestResponse>>()};
-    add_test_message(backend.get(), MESSAGE);
+  TestServiceImpl service{};
 
-    EXPECT_CALL(stub, server_streamRaw(_, _)).WillOnce(Return(backend.release()));
+  std::shared_ptr<grpc::Channel> channel{};
+  std::unique_ptr<TestService::Stub> client_stub{};
+
+  void SetUp() override {
+    DLOG(INFO) << "PubSubServiceTest::SetUp()";
+    grpc::ServerBuilder builder;
+    int port{};
+    builder.AddListeningPort("localhost:0", grpc::InsecureServerCredentials(), &port);
+    builder.RegisterService(&service);
+    server = builder.BuildAndStart();
+
+    DLOG(INFO) << "PubSubServiceTest::SetUp() -- Listening on port " << port;
+
+    server_address << "localhost:" << port;
+    channel = grpc::CreateChannel(server_address.str(), grpc::InsecureChannelCredentials());
+    client_stub = TestService::NewStub(channel);
+
+    DLOG(INFO) << "PubSubServiceTest::SetUp() -- Client connecting to " << server_address.str();
   }
 
+  void TearDown() override {
+    client_stub.reset();
+    channel.reset();
+    server->Shutdown();
+  }
+
+  void add_canned_response(std::string_view text) {
+    TestResponse response{};
+    response.set_message(std::string{text});
+    service.canned_responses.emplace_back(std::move(response));
+  }
+};
+
+TEST_F(PubSubServiceTest, CanPublishRpcResponseOverTopic) {
+  constexpr char MESSAGE[]{"message"};
+  add_canned_response(MESSAGE);
+
   TestTopic<TestResponse> topic{};
-  PubSubService<TestRequest, TestResponse> service{
-      topic, [&stub](grpc::ClientContext* context, const TestRequest& request) {
-        return stub.server_stream(context, request);
+  PubSubService<TestRequest, TestResponse> pub_sub{
+      topic, [this](grpc::ClientContext* context, const TestRequest* request,
+                    grpc::ClientReadReactor<TestResponse>* reactor) {
+        DLOG(INFO) << "Initiating connection to server.";
+        return this->client_stub->async()->server_stream(context, request, reactor);
       }};
 
-  service.loop();
+  pub_sub.start();
+  pub_sub.await();
 
+  EXPECT_TRUE(pub_sub.status().ok());
   EXPECT_EQ(MESSAGE, topic.last_message().message());
 }
 
-TEST(PubSubServiceTest, CanPublishRpcStreamOverTopic) {
-  std::vector<TestResponse> expected{};
+TEST_F(PubSubServiceTest, CanPublishRpcStreamOverTopic) {
   for (int i = 0; i < 10; ++i) {
     using std::to_string;
-    TestResponse message{};
-    message.set_message("Message " + to_string(i));
-    expected.emplace_back(message);
-  }
-
-  MockTestServiceStub stub;
-
-  {
-    std::unique_ptr<TestReader<TestResponse>> backend{std::make_unique<TestReader<TestResponse>>()};
-    for (const auto& msg : expected) {
-      backend->add_response(msg);
-    }
-
-    EXPECT_CALL(stub, server_streamRaw(_, _)).WillOnce(Return(backend.release()));
+    add_canned_response("Message " + to_string(i));
   }
 
   TestTopic<TestResponse> topic{};
@@ -104,16 +144,49 @@ TEST(PubSubServiceTest, CanPublishRpcStreamOverTopic) {
   topic.register_callback(
       [&responses](const TestResponse& message) { responses.emplace_back(message); });
 
-  PubSubService<TestRequest, TestResponse> service{
-      topic, [&stub](grpc::ClientContext* context, const TestRequest& request) {
-        return stub.server_stream(context, request);
+  PubSubService<TestRequest, TestResponse> pub_sub{
+      topic, [this](grpc::ClientContext* context, const TestRequest* request,
+                    grpc::ClientReadReactor<TestResponse>* reactor) {
+        return this->client_stub->async()->server_stream(context, request, reactor);
       }};
 
-  service.loop();
+  pub_sub.start();
+  pub_sub.await();
 
-  EXPECT_EQ(expected.size(), responses.size());
+  EXPECT_TRUE(pub_sub.status().ok());
+  EXPECT_EQ(service.canned_responses.size(), responses.size());
   for (uint32_t i = 0; i < responses.size(); ++i) {
-    EXPECT_TRUE(google::protobuf::util::MessageDifferencer::Equivalent(expected[i], responses[i]));
+    EXPECT_TRUE(google::protobuf::util::MessageDifferencer::Equivalent(service.canned_responses[i],
+                                                                       responses[i]));
+  }
+}
+
+TEST_F(PubSubServiceTest, CanPublishLongRpcStreamOverTopic) {
+  for (int i = 0; i < 50; ++i) {
+    using std::to_string;
+    add_canned_response("Message " + to_string(i));
+  }
+
+  TestTopic<TestResponse> topic{};
+
+  std::vector<TestResponse> responses{};
+  topic.register_callback(
+      [&responses](const TestResponse& message) { responses.emplace_back(message); });
+
+  PubSubService<TestRequest, TestResponse> pub_sub{
+      topic, [this](grpc::ClientContext* context, const TestRequest* request,
+                    grpc::ClientReadReactor<TestResponse>* reactor) {
+        return this->client_stub->async()->server_stream(context, request, reactor);
+      }};
+
+  pub_sub.start();
+  pub_sub.await();
+
+  EXPECT_TRUE(pub_sub.status().ok());
+  EXPECT_EQ(service.canned_responses.size(), responses.size());
+  for (uint32_t i = 0; i < responses.size(); ++i) {
+    EXPECT_TRUE(google::protobuf::util::MessageDifferencer::Equivalent(service.canned_responses[i],
+                                                                       responses[i]));
   }
 }
 
