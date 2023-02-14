900f3c266957293a8bf504c1f03aac5e875a345b
James Pack <jgrimm@gmail.com>
2023-02-14 13:04:53 -0500

    Move the list_devices utility under the //radio package and remove it from the //service/radio Debian package.

--- radio/list_devices.cc
+++ radio/list_devices.cc
@@ -4,10 +4,10 @@
 #include "glog/logging.h"
 #include "radio/soapy.h"
 
-namespace tvsc::service::radio::server {
+namespace tvsc::radio {
 
 void list_devices() {
-  tvsc::radio::Soapy soapy{};
+  Soapy soapy{};
 
   std::cout << "Soapy modules:\n";
   for (const auto& module : soapy.modules()) {
@@ -18,19 +18,15 @@ void list_devices() {
   for (const auto& device : soapy.device_names()) {
     std::cout << "\t" << device << "\n";
   }
-
-  LOG_IF(ERROR, !soapy.contains_module("libdummy_radio.so")) << "'dummy_radio' module not found";
-
-  LOG_IF(ERROR, !soapy.has_device("dummy_receiver")) << "'dummy_receiver' device not found";
 }
 
-}  // namespace tvsc::service::radio::server
+}  // namespace tvsc::radio
 
 int main(int argc, char** argv) {
   google::InitGoogleLogging(argv[0]);
   gflags::ParseCommandLineFlags(&argc, &argv, true);
 
-  tvsc::service::radio::server::list_devices();
+  tvsc::radio::list_devices();
 
   return 0;
 }
