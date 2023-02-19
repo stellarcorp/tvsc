555903d962b6bbdc3fdaff3335a159dbfe655952
James Pack <jgrimm@gmail.com>
2023-02-19 15:54:21 -0500

    Add a utility function to help generate random numbers more easily.

---
+++ random/random.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <limits>
+#include <random>
+
+namespace tvsc::random {
+
+template <typename ResultT>
+ResultT generate_random_value(ResultT minimum = std::numeric_limits<ResultT>::lowest(),
+                              ResultT maximum = std::numeric_limits<ResultT>::max());
+
+template <>
+uint64_t generate_random_value(uint64_t minimum, uint64_t maximum) {
+  static std::random_device random_device{};
+  static std::default_random_engine generator{random_device()};
+  std::uniform_int_distribution<> distribution(minimum, maximum);
+  return distribution(generator);
+}
+
+}  // namespace tvsc::random
