f8ee1cb8a59be9501dc8becb4fd39e867beb8627
James Pack <james.pack@stellarcorp.tv>
2023-06-21 15:08:22 -0400

    Update pin mapping for Teensy 4.0 boards. We moved the interrupt pin to pin 14, since it is easier to verify that it is in the correct place.

--- radio/single_radio_pin_mapping_teensy40_default.cc
+++ radio/single_radio_pin_mapping_teensy40_default.cc
@@ -5,6 +5,6 @@ namespace tvsc::radio {
 const char* SingleRadioPinMapping::board_name() { return "RF69HCW 433 Teensy40"; }
 uint8_t SingleRadioPinMapping::reset_pin() { return 9; }
 uint8_t SingleRadioPinMapping::chip_select_pin() { return 10; }
-uint8_t SingleRadioPinMapping::interrupt_pin() { return 17; }
+uint8_t SingleRadioPinMapping::interrupt_pin() { return 14; }
 
 }  // namespace tvsc::radio
