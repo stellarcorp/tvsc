e0b9ab990797f159a5f0cc595790094ed7e4ffef
James Pack <james.pack@stellarcorp.tv>
2023-06-27 09:58:11 -0400

    Integrate standard initialization routine into test main to help ensure that the test environment more closely duplicates the production setup.

--- third_party/gtest/testing_main.cc
+++ third_party/gtest/testing_main.cc
@@ -1,28 +1,10 @@
 #include <cstdio>
 
+#include "base/initializer.h"
 #include "gflags/gflags.h"
 #include "glog/logging.h"
 #include "gtest/gtest.h"
 #include "testing/test_temp_directory.h"
-
-#if GTEST_OS_ESP8266 || GTEST_OS_ESP32
-#if GTEST_OS_ESP8266
-extern "C" {
-#endif
-void setup() {
-  testing::InitGoogleTest();
-  google::InitGoogleLogging();
-  google::InstallFailureSignalHandler();
-  ::testing::AddGlobalTestEnvironment(pack::testing::TestTempDirectory::getInstance());
-}
-
-void loop() { RUN_ALL_TESTS(); }
-
-#if GTEST_OS_ESP8266
-}
-#endif
-
-#else
 
 GTEST_API_ int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
@@ -30,10 +12,8 @@ GTEST_API_ int main(int argc, char **argv) {
   // Default logging configuration for tests.
   FLAGS_logtostderr = true;
   FLAGS_stderrthreshold = 0;
-  google::InitGoogleLogging(argv[0]);
 
-  // But allow user to override via the command line.
-  gflags::ParseCommandLineFlags(&argc, &argv, true);
+  tvsc::initialize(&argc, &argv);
 
   google::InstallFailureSignalHandler();
 
@@ -41,5 +21,3 @@ GTEST_API_ int main(int argc, char **argv) {
 
   return RUN_ALL_TESTS();
 }
-
-#endif
