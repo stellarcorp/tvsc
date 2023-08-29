931f3172a8447354153aae2b5a097d8c281dc3b4
James Pack <james.pack@stellarcorp.tv>
2023-08-29 15:25:52 -0400

    Add increment method to MockClock

--- hal/time/mock_clock.cc
+++ hal/time/mock_clock.cc
@@ -5,11 +5,17 @@
 namespace tvsc::hal::time {
 
 uint64_t MockClock::current_time_millis() { return current_time_ms_; }
+
 void MockClock::set_current_time_millis(uint64_t current_time_ms) {
   current_time_ms_ = current_time_ms;
 }
 
+void MockClock::increment_current_time_millis(uint64_t increment_ms) {
+  current_time_ms_ += increment_ms;
+}
+
 void MockClock::sleep_ms(uint32_t t) { current_time_ms_ += t; }
+
 void MockClock::sleep_us(uint32_t t) {
   if (t < 1000) {
     // Force a minimum sleep time of 1ms, otherwise, this call will often behave like a no-op and
--- hal/time/mock_clock.h
+++ hal/time/mock_clock.h
@@ -11,6 +11,7 @@ class MockClock final : public Clock {
  public:
   uint64_t current_time_millis() override;
   void set_current_time_millis(uint64_t current_time_ms);
+  void increment_current_time_millis(uint64_t increment_ms = 1);
 
   void sleep_ms(uint32_t milliseconds) override;
   void sleep_us(uint32_t microseconds) override;
