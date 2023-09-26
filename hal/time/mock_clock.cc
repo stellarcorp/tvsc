a776d040db7c964ce3aa6d7216ebc033d6215085
James Pack <jgrimm@gmail.com>
2023-09-26 12:08:33 -0400

    Add concept for updating mocks of async systems (radio modules, etc.) whenever the MockClock is updated.

--- hal/time/mock_clock.cc
+++ hal/time/mock_clock.cc
@@ -9,18 +9,28 @@ uint64_t MockClock::current_time_millis() { return current_time_us_ / 1000; }
 
 void MockClock::set_current_time_millis(uint64_t current_time_ms) {
   current_time_us_ = current_time_ms * 1000;
+  update_clockables();
 }
 
 void MockClock::increment_current_time_millis(uint64_t increment_ms) {
   current_time_us_ += increment_ms * 1000;
+  update_clockables();
 }
 
 void MockClock::set_current_time_micros(uint64_t current_time_us) {
   current_time_us_ = current_time_us;
+  update_clockables();
 }
 
 void MockClock::increment_current_time_micros(uint64_t increment_us) {
   current_time_us_ += increment_us;
+  update_clockables();
+}
+
+void MockClock::update_clockables() {
+  for (Clockable* clockable : clockables_) {
+    clockable->update(current_time_us_);
+  }
 }
 
 void MockClock::sleep_ms(uint32_t t) { increment_current_time_millis(t); }
--- hal/time/mock_clock.h
+++ hal/time/mock_clock.h
@@ -1,14 +1,25 @@
+#pragma once
+
 #include <cstdint>
+#include <vector>
 
 #include "hal/time/clock.h"
 
 namespace tvsc::hal::time {
 
+class Clockable;
+
 class MockClock final : public Clock {
  private:
   uint64_t current_time_us_{};
 
+  std::vector<Clockable*> clockables_{};
+
+  void update_clockables();
+
  public:
+  void register_clockable(Clockable& clockable) { clockables_.push_back(&clockable); }
+
   uint64_t current_time_millis() override;
   uint64_t current_time_micros() override;
 
@@ -21,6 +32,23 @@ class MockClock final : public Clock {
 
   void set_current_time_micros(uint64_t current_time_us);
   void increment_current_time_micros(uint64_t increment_us = 1);
+};
+
+/**
+ * A Clockable is a test-only mock that represents a parallel processing subsystem, such as a
+ * daughterboard or external chipset. A Clockable registers itself with the MockClock, and then any
+ * subsequent calls to set the time result in a call to update the Clockable. The Clockable should
+ * then modify its state to reflect the new time.
+ */
+class Clockable {
+ protected:
+  MockClock* clock_;
+
+ public:
+  Clockable(MockClock& clock) : clock_(&clock) { clock_->register_clockable(*this); }
+  virtual ~Clockable() = default;
+
+  virtual void update(uint64_t current_time_us) = 0;
 };
 
 }  // namespace tvsc::hal::time
