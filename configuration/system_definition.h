76c73edf8af4a2f23f1f0f3c6111e45feffbac85
James Pack <jgrimm@gmail.com>
2023-10-04 13:01:11 -0400

    Created package for handling system configuration. This will eventually replace the radio_configuration.h scheme in //comms/radio.

---
+++ configuration/system_definition.h
@@ -0,0 +1,251 @@
+#pragma once
+
+#include <algorithm>
+#include <initializer_list>
+#include <type_traits>
+#include <utility>
+#include <variant>
+#include <vector>
+
+namespace tvsc::configuration {
+
+using SystemId = uint8_t;
+using ComponentId = uint8_t;
+using FunctionId = uint8_t;
+
+// Note that many types and functions related to settings can be made 'constexpr' in C++20. The
+// settings package makes heavy use of std::vector. This class gets constexpr constructors,
+// iterators and other enabling features starting in C++20.
+#if __cplusplus >= 202000
+#define CONSTEXPR_SETTINGS constexpr
+#else
+#define CONSTEXPR_SETTINGS
+#endif
+
+template <typename EnumType>
+constexpr auto as_int(EnumType value) {
+  return static_cast<std::underlying_type_t<EnumType>>(value);
+}
+
+template <typename EnumType>
+constexpr auto as_int(EnumType* value) {
+  return reinterpret_cast<std::underlying_type_t<EnumType>*>(value);
+}
+
+template <typename EnumType>
+constexpr auto as_int(const EnumType* value) {
+  return reinterpret_cast<const std::underlying_type_t<EnumType>*>(value);
+}
+
+class AllowedValues final {
+ public:
+  using StoredT = std::variant<int32_t, int64_t, float, double>;
+
+ private:
+  std::vector<StoredT> enumerated_{};
+
+ public:
+  constexpr AllowedValues() = default;
+
+  CONSTEXPR_SETTINGS AllowedValues(std::initializer_list<int32_t> allowed_values)
+      : enumerated_(allowed_values.begin(), allowed_values.end()) {}
+
+  CONSTEXPR_SETTINGS AllowedValues(std::initializer_list<int64_t> allowed_values)
+      : enumerated_(allowed_values.begin(), allowed_values.end()) {}
+
+  CONSTEXPR_SETTINGS AllowedValues(std::initializer_list<float> allowed_values)
+      : enumerated_(allowed_values.begin(), allowed_values.end()) {}
+
+  CONSTEXPR_SETTINGS AllowedValues(std::initializer_list<double> allowed_values)
+      : enumerated_(allowed_values.begin(), allowed_values.end()) {}
+
+  template <typename EnumT>
+  CONSTEXPR_SETTINGS AllowedValues(std::initializer_list<EnumT> allowed_values)
+      : enumerated_(as_int(allowed_values.begin()), as_int(allowed_values.end())) {}
+
+  AllowedValues(const AllowedValues& rhs) = default;
+  AllowedValues(AllowedValues&& rhs) = default;
+  AllowedValues& operator=(const AllowedValues& rhs) = default;
+  AllowedValues& operator=(AllowedValues&& rhs) = default;
+
+  CONSTEXPR_SETTINGS bool is_allowed(int32_t value) const {
+    for (const auto& v : enumerated_) {
+      if (std::get<int32_t>(v) == value) return true;
+    }
+    return false;
+  }
+
+  CONSTEXPR_SETTINGS bool is_allowed(int64_t value) const {
+    for (const auto& v : enumerated_) {
+      if (std::get<int64_t>(v) == value) return true;
+    }
+    return false;
+  }
+
+  CONSTEXPR_SETTINGS bool is_allowed(float value) const {
+    for (const auto& v : enumerated_) {
+      if (std::get<float>(v) == value) return true;
+    }
+    return false;
+  }
+
+  CONSTEXPR_SETTINGS bool is_allowed(double value) const {
+    for (const auto& v : enumerated_) {
+      if (std::get<double>(v) == value) return true;
+    }
+    return false;
+  }
+
+  template <typename ValueT>
+  CONSTEXPR_SETTINGS bool is_allowed(ValueT value) const {
+    static_assert(std::is_enum<ValueT>::value);
+    for (const auto& v : enumerated_) {
+      if (std::get<std::underlying_type_t<ValueT>>(v) == std::underlying_type_t<ValueT>(value))
+        return true;
+    }
+    return false;
+  }
+
+  constexpr const std::vector<StoredT>& enumerated_values() const { return enumerated_; }
+};
+
+class Function final {
+ private:
+  FunctionId function_id_;
+
+  AllowedValues allowed_values_;
+
+ public:
+  template <typename ValueT>
+  CONSTEXPR_SETTINGS Function(FunctionId function_id, std::initializer_list<ValueT> allowed_values)
+      : function_id_(function_id), allowed_values_(allowed_values) {}
+
+  CONSTEXPR_SETTINGS Function(FunctionId function_id, AllowedValues allowed_values)
+      : function_id_(function_id), allowed_values_(allowed_values) {}
+
+  Function(const Function& rhs) = default;
+  Function(Function&& rhs) = default;
+  Function& operator=(const Function& rhs) = default;
+  Function& operator=(Function&& rhs) = default;
+
+  /**
+   * Functions are identified by their function_id. These methods
+   * define that identification scheme.
+   *
+   * Also, functions are ordered for faster lookup. These methods define that ordering.
+   */
+  constexpr operator FunctionId() const { return identifier(); }
+
+  constexpr bool operator<(const Function& rhs) const { return identifier() < rhs.identifier(); }
+  constexpr bool operator==(const Function& rhs) const { return identifier() == rhs.identifier(); }
+
+  constexpr FunctionId function_id() const { return function_id_; }
+
+  constexpr FunctionId identifier() const { return function_id(); }
+
+  template <typename ValueT>
+  CONSTEXPR_SETTINGS bool is_allowed(const ValueT& value) const {
+    return allowed_values_.is_allowed(value);
+  }
+
+  constexpr const AllowedValues& allowed_values() const { return allowed_values_; }
+};
+
+class Component final {
+ private:
+  ComponentId component_id_;
+  std::vector<Function> functions_;
+
+ public:
+  CONSTEXPR_SETTINGS Component(ComponentId component_id, std::initializer_list<Function> functions)
+      : component_id_(component_id), functions_(functions.begin(), functions.end()) {
+    // Note that std::sort() is constexpr starting in C++20.
+    std::sort(functions_.begin(), functions_.end());
+  }
+
+  constexpr operator ComponentId() const { return identifier(); }
+  constexpr ComponentId identifier() const { return component_id_; }
+
+  CONSTEXPR_SETTINGS const std::vector<Function>& functions() const { return functions_; }
+
+  CONSTEXPR_SETTINGS bool has_function(FunctionId function_id) const {
+    return std::binary_search(functions_.begin(), functions_.end(), function_id);
+  }
+
+  CONSTEXPR_SETTINGS std::pair<bool, const Function*> search(FunctionId function_id) const {
+    auto iter{std::lower_bound(functions_.begin(), functions_.end(), function_id)};
+    if (iter == functions_.end() || iter->identifier() != function_id) {
+      return {false, nullptr};
+    } else {
+      return {true, &(*iter)};
+    }
+  }
+};
+
+class System final {
+ private:
+  SystemId system_;
+  std::vector<System> subsystems_{};
+  std::vector<Component> components_{};
+
+ public:
+  CONSTEXPR_SETTINGS System(SystemId system) : system_(system) {}
+
+  CONSTEXPR_SETTINGS System(SystemId system, std::initializer_list<System> subsystems)
+      : system_(system), subsystems_(subsystems.begin(), subsystems.end()) {
+    // Note that std::sort() is constexpr starting in C++20.
+    std::sort(subsystems_.begin(), subsystems_.end());
+  }
+
+  CONSTEXPR_SETTINGS System(SystemId system, std::initializer_list<Component> components)
+      : system_(system), components_(components.begin(), components.end()) {
+    // Note that std::sort() is constexpr starting in C++20.
+    std::sort(components_.begin(), components_.end());
+  }
+
+  CONSTEXPR_SETTINGS System(SystemId system, std::initializer_list<System> subsystems,
+                            std::initializer_list<Component> components)
+      : system_(system),
+        subsystems_(subsystems.begin(), subsystems.end()),
+        components_(components.begin(), components.end()) {
+    // Note that std::sort() is constexpr starting in C++20.
+    std::sort(subsystems_.begin(), subsystems_.end());
+    std::sort(components_.begin(), components_.end());
+  }
+
+  constexpr operator SystemId() const { return identifier(); }
+  constexpr SystemId identifier() const { return system_; }
+
+  CONSTEXPR_SETTINGS const std::vector<System>& subsystems() const { return subsystems_; }
+
+  CONSTEXPR_SETTINGS bool has_subsystem(SystemId subsystem_id) const {
+    return std::binary_search(subsystems_.begin(), subsystems_.end(), subsystem_id);
+  }
+
+  CONSTEXPR_SETTINGS std::pair<bool, const System*> search_subsystems(SystemId subsystem_id) const {
+    auto iter{std::lower_bound(subsystems_.begin(), subsystems_.end(), subsystem_id)};
+    if (iter == subsystems_.end() || iter->identifier() != subsystem_id) {
+      return {false, nullptr};
+    } else {
+      return {true, &(*iter)};
+    }
+  }
+
+  CONSTEXPR_SETTINGS const std::vector<Component>& components() const { return components_; }
+
+  CONSTEXPR_SETTINGS bool has_component(ComponentId component_id) const {
+    return std::binary_search(components_.begin(), components_.end(), component_id);
+  }
+
+  CONSTEXPR_SETTINGS std::pair<bool, const Component*> search_components(
+      ComponentId component_id) const {
+    auto iter{std::lower_bound(components_.begin(), components_.end(), component_id)};
+    if (iter == components_.end() || iter->identifier() != component_id) {
+      return {false, nullptr};
+    } else {
+      return {true, &(*iter)};
+    }
+  }
+};
+
+}  // namespace tvsc::configuration
---
+++ configuration/system_definition_usability_test.cc
@@ -0,0 +1,179 @@
+#include "configuration/system_definition.h"
+#include "gtest/gtest.h"
+
+namespace tvsc::configuration {
+
+enum class Systems : SystemId {
+  NAVIGATION,
+  POWER,
+  COMMUNICATIONS,
+};
+
+enum class CommunicationsSubsystems : SystemId {
+  LINK_MANAGEMENT,
+  MULTIPLE_ACCESS,
+  TRANSCEIVER,
+};
+
+enum class TransceiverSubsystems : SystemId {
+  HALF_DUPLEX_RADIO,
+  SIMPLEX_TX_RADIO,
+  SIMPLEX_RX_RADIO,
+  DUPLEX_RADIO,
+  LOW_NOISE_AMPLIFIER,
+  OSCILLATOR,
+};
+
+enum class RadioSettings : FunctionId {
+  MODULATION_SCHEME,
+  LINE_CODING,
+  TX_POWER,
+};
+
+enum class ModulationScheme {
+  CW,
+  OOK,
+  FSK,
+  LORA,
+};
+
+enum class LineCoding {
+  NONE,
+
+  WHITENING,
+
+  // This list primarily comes from:
+  // https://en.wikipedia.org/wiki/Line_code#Transmission_and_storage
+  NRZ_L,
+  NRZ_M,
+  NRZ_S,
+  RZ,
+  BIPHASE_L,
+  BIPHASE_M,
+  BIPHASE_S,
+
+  // Manchester encoding and differential Manchester encoding both have multiple implementations.
+  MANCHESTER_ORIGINAL,
+  MANCHESTER_802_3,
+
+  DIFFERENTIAL_MANCHESTER,
+
+  BIPOLAR,
+};
+
+template <typename T>
+constexpr T as_constant(T value) {
+  return value;
+}
+
+// static constexpr auto allowed_modulation_schemes = {};
+
+Function modulation_scheme{as_int(RadioSettings::MODULATION_SCHEME),
+                           {ModulationScheme::FSK, ModulationScheme::OOK}};
+
+Function line_coding{as_int(RadioSettings::LINE_CODING),
+                     {LineCoding::NRZ_L, LineCoding::MANCHESTER_802_3}};
+
+static constexpr ComponentId RADIO_1{1};
+static CONSTEXPR_SETTINGS Component radio_1{RADIO_1,
+                                            {
+                                                modulation_scheme,
+                                                line_coding,
+                                            }};
+
+TEST(SettingsUsabilityTest, CanTestAllowedValueForFunction) {
+#if __cplusplus >= 202000
+  // We expect to be able to check if particular values are allowed at compile time in C++20 and
+  // later.
+  static_assert(modulation_scheme.is_allowed(ModulationScheme::OOK));
+  static_assert(modulation_scheme.is_allowed(ModulationScheme::FSK));
+#endif
+
+  EXPECT_TRUE(modulation_scheme.is_allowed(ModulationScheme::OOK));
+  EXPECT_TRUE(modulation_scheme.is_allowed(ModulationScheme::FSK));
+  EXPECT_FALSE(modulation_scheme.is_allowed(ModulationScheme::LORA));
+}
+
+TEST(SettingsUsabilityTest, CanFindFunctionInComponentById) {
+#if __cplusplus >= 202000
+  static_assert(*radio_1.search(RadioSettings::MODULATION_SCHEME).second == modulation_scheme);
+  static_assert(*radio_1.search(RadioSettings::LINE_CODING).second == line_coding);
+#endif
+
+  EXPECT_EQ(modulation_scheme, *radio_1.search(as_int(RadioSettings::MODULATION_SCHEME)).second);
+  EXPECT_EQ(line_coding, *radio_1.search(as_int(RadioSettings::LINE_CODING)).second);
+  EXPECT_FALSE(radio_1.search(as_int(RadioSettings::TX_POWER)).first);
+  EXPECT_EQ(nullptr, radio_1.search(as_int(RadioSettings::TX_POWER)).second);
+}
+
+TEST(SettingsUsabilityTest, CanFindComponentsInSystem) {
+  static CONSTEXPR_SETTINGS System trivial_transceiver_system{
+      as_int(CommunicationsSubsystems::TRANSCEIVER),
+      {radio_1},
+  };
+
+#if __cplusplus >= 202000
+  static_assert(*trivial_transceiver_system.search_components(RADIO_1).second == radio_1);
+#endif
+
+  EXPECT_EQ(radio_1, *trivial_transceiver_system.search_components(RADIO_1).second);
+}
+
+TEST(SettingsUsabilityTest, CanFindSubsystemsInSystem) {
+  static CONSTEXPR_SETTINGS System full_transceiver_system{
+      as_int(CommunicationsSubsystems::TRANSCEIVER),
+      {
+          {as_int(TransceiverSubsystems::HALF_DUPLEX_RADIO), {radio_1}},
+      },
+  };
+
+#if __cplusplus >= 202000
+  static_assert(
+      full_transceiver_system.search_subsystems(as_int(TransceiverSubsystems::HALF_DUPLEX_RADIO))
+          .second->identifier() == as_int(TransceiverSubsystems::HALF_DUPLEX_RADIO));
+#endif
+
+  EXPECT_EQ(
+      as_int(TransceiverSubsystems::HALF_DUPLEX_RADIO),
+      full_transceiver_system.search_subsystems(as_int(TransceiverSubsystems::HALF_DUPLEX_RADIO))
+          .second->identifier());
+}
+
+TEST(SettingsUsabilityTest, CanDefineComplexSystems) {
+  static constexpr SystemId SATELLITE_ID{42};
+  static CONSTEXPR_SETTINGS System satellite{
+      SATELLITE_ID,
+      {
+          {as_int(Systems::NAVIGATION)},
+          {as_int(Systems::POWER)},
+          {
+              as_int(Systems::COMMUNICATIONS),
+              {
+                  as_int(CommunicationsSubsystems::TRANSCEIVER),
+                  {
+                      as_int(TransceiverSubsystems::HALF_DUPLEX_RADIO),
+                      {radio_1},
+                  },
+              },
+          },
+      },
+  };
+
+#if __cplusplus >= 202000
+  static_assert(satellite.search_subsystems(as_int(Systems::NAVIGATION)).second->identifier() ==
+                as_int(Systems::NAVIGATION));
+  static_assert(satellite.search_subsystems(as_int(Systems::POWER)).second->identifier() ==
+                as_int(Systems::POWER));
+  static_assert(satellite.search_subsystems(as_int(Systems::COMMUNICATIONS)).second->identifier() ==
+                as_int(Systems::COMMUNICATIONS));
+#endif
+
+  EXPECT_EQ(as_int(Systems::NAVIGATION),
+            satellite.search_subsystems(as_int(Systems::NAVIGATION)).second->identifier());
+  EXPECT_EQ(as_int(Systems::POWER),
+            satellite.search_subsystems(as_int(Systems::POWER)).second->identifier());
+  EXPECT_EQ(as_int(Systems::COMMUNICATIONS),
+            satellite.search_subsystems(as_int(Systems::COMMUNICATIONS)).second->identifier());
+}
+
+}  // namespace tvsc::configuration
