1589beb5ecd70bd0e4e84ae33553332bd300596b
James Pack <james.pack@stellarcorp.tv>
2023-10-07 11:47:37 -0400

    Remove extraneous print statements.

--- configuration/function_test.cc
+++ configuration/function_test.cc
@@ -1,5 +1,3 @@
-#include <iostream>
-
 #include "configuration/system_definition.h"
 #include "configuration/fake_systems.h"
 #include "gtest/gtest.h"
@@ -7,17 +5,12 @@
 namespace tvsc::configuration {
 
 TEST(FunctionTest, CanTestAllowedValue) {
-  std::cout << to_string(modulation_scheme_values) << "\n";
-
   EXPECT_TRUE(modulation_scheme_values.is_allowed(ModulationScheme::OOK));
   EXPECT_TRUE(modulation_scheme_values.is_allowed(ModulationScheme::FSK));
   EXPECT_FALSE(modulation_scheme_values.is_allowed(ModulationScheme::LORA));
 }
 
 TEST(FunctionTest, CanTestAllowedValueForFunction) {
-  std::cout << to_string(modulation_scheme_values) << "\n";
-  std::cout << to_string(modulation_scheme) << "\n";
-
   EXPECT_TRUE(modulation_scheme.is_allowed(ModulationScheme::OOK));
   EXPECT_TRUE(modulation_scheme.is_allowed(ModulationScheme::FSK));
   EXPECT_FALSE(modulation_scheme.is_allowed(ModulationScheme::LORA));
