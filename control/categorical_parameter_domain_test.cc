a39a646d6e4d4b908c35d9c72aea9174933ee753
James Pack <james.pack@stellarcorp.tv>
2024-02-05 11:07:39 -0500

    Categorical domain working, if simplistic

--- control/categorical_parameter_domain_test.cc
+++ control/categorical_parameter_domain_test.cc
@@ -12,4 +12,22 @@ TEST(CategoricalParameterDomainTest, CanDetectAllowedValue) {
   EXPECT_FALSE(domain->in_domain(4));
 }
 
+TEST(CategoricalParameterDomainTest, CanAddValuesMultipleTimes) {
+  auto domain = configure_categorical_domain<int>().with_values(1, 3).with_values(5, 6, 7, 8).create();
+  EXPECT_TRUE(domain->in_domain(1));
+  EXPECT_TRUE(domain->in_domain(3));
+  EXPECT_TRUE(domain->in_domain(5));
+  EXPECT_TRUE(domain->in_domain(6));
+  EXPECT_TRUE(domain->in_domain(7));
+  EXPECT_TRUE(domain->in_domain(8));
+  EXPECT_FALSE(domain->in_domain(0));
+  EXPECT_FALSE(domain->in_domain(2));
+  EXPECT_FALSE(domain->in_domain(4));
+}
+
+TEST(CategoricalParameterDomainTest, SizeIsNumberOfValues) {
+  auto domain = configure_categorical_domain<int>().with_values(1, 3).create();
+  EXPECT_EQ(2, domain->size());
+}
+
 }  // namespace tvsc::control
