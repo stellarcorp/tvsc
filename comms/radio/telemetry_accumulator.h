66b529035f7dad2eb347d0d01cde05dc03130936
James Pack <jgrimm@gmail.com>
2024-12-03 06:36:36 -0500

    Removed implementations inside of TelemetryAccumulator. That concept is generally valid, but the implementations need to be thought through now that we are not using protobufs on embedded systems.

--- comms/radio/telemetry_accumulator.h
+++ comms/radio/telemetry_accumulator.h
@@ -1,40 +1,19 @@
 #pragma once
 
-#include "comms/radio/nanopb_proto/settings.pb.h"
+#include <cstdint>
+#include <string>
+
+#include "comms/radio/transceiver_identification.h"
 
 namespace tvsc::comms::radio {
 
 class TelemetryAccumulator final {
  private:
-  tvsc_comms_radio_nano_TelemetryReport report_{};
-
   void update_time_measurement();
   void update_owned_metrics();
 
  public:
-  static const tvsc_comms_radio_nano_TelemetryEvent* find_event(
-      const tvsc_comms_radio_nano_TelemetryReport& report, tvsc_comms_radio_nano_Metric metric) {
-    for (pb_size_t index = 0; index < report.events_count; ++index) {
-      const tvsc_comms_radio_nano_TelemetryEvent& event{report.events[index]};
-      if (event.metric == metric) {
-        return &event;
-      }
-    }
-    return nullptr;
-  }
-
-  static tvsc_comms_radio_nano_TelemetryEvent* find_event(tvsc_comms_radio_nano_TelemetryReport& report,
-                                                    tvsc_comms_radio_nano_Metric metric) {
-    for (pb_size_t index = 0; index < report.events_count; ++index) {
-      tvsc_comms_radio_nano_TelemetryEvent& event{report.events[index]};
-      if (event.metric == metric) {
-        return &event;
-      }
-    }
-    return nullptr;
-  }
-
-  TelemetryAccumulator(uint32_t device_id);
+  TelemetryAccumulator(const TransceiverIdentification& device_id);
 
   void increment_fragments_received();
   void increment_fragments_transmitted();
@@ -52,7 +31,8 @@ class TelemetryAccumulator final {
    */
   void set_power_usage_w(float power_w);
 
-  const tvsc_comms_radio_nano_TelemetryReport& generate_telemetry_report();
+  // TODO(james): Replace with method for generating a packet or fragment for transmission.
+  std::string generate_report_string();
 };
 
 }  // namespace tvsc::comms::radio
