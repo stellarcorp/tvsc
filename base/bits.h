241ad3d503ca77d2537c5b9899de6a6f46701f2d
James Pack <jgrimm@gmail.com>
2025-01-11 09:20:12 -0500

    Added functionality for working with bit fields.

--- base/bits.h
+++ base/bits.h
@@ -3,9 +3,9 @@
 #include <cstdint>
 
 #ifdef __has_include
-#  if __has_include(<bit>)
-#    include <bit>
-#  endif
+#if __has_include(<bit>)
+#include <bit>
+#endif
 #endif
 
 namespace tvsc {
@@ -29,6 +29,114 @@ inline constexpr std::size_t number_of_bits(std::size_t x) noexcept {
  */
 inline constexpr uint8_t bit_width(std::size_t value) {
   return static_cast<uint8_t>(internal::number_of_bits(value));
+}
+
+template <uint8_t NUM_BITS, uint8_t BIT_FIELD_OFFSET>
+inline constexpr uint32_t compute_bit_mask() {
+  static_assert(NUM_BITS + BIT_FIELD_OFFSET <= 32,
+                "Invalid mask parameters. The total number of bits (offset and the number "
+                "of bits in the mask) must be less than or equal to the size of the word "
+                "which is 32 bits. Likely, this is a typo or other scrivener's error in the "
+                "template parameters to call this method.");
+  return ((1UL << NUM_BITS) - 1UL) << BIT_FIELD_OFFSET;
+}
+
+template <uint8_t NUM_BITS>
+inline constexpr uint32_t compute_bit_mask(uint8_t bit_field_offset) {
+  return ((1UL << NUM_BITS) - 1UL) << bit_field_offset;
+}
+
+inline constexpr uint32_t compute_bit_mask(uint8_t num_bits, uint8_t bit_field_offset) {
+  return ((1UL << num_bits) - 1UL) << bit_field_offset;
+}
+
+template <uint8_t NUM_BITS, uint8_t BIT_FIELD_OFFSET>
+inline constexpr uint32_t get_bit_field_value(const uint32_t& value) {
+  static_assert(NUM_BITS + BIT_FIELD_OFFSET <= 32,
+                "Invalid bit field parameters. The total number of bits (offset and the number "
+                "of bits in the field) must be less than or equal to the size of the register "
+                "which is 32 bits. Likely, this is a typo or other scrivener's error in the "
+                "template parameters to call this method.");
+  constexpr uint32_t MASK{compute_bit_mask<NUM_BITS, BIT_FIELD_OFFSET>()};
+  return (value & MASK) >> BIT_FIELD_OFFSET;
+}
+
+template <uint8_t NUM_BITS, uint8_t BIT_FIELD_OFFSET>
+inline uint32_t get_bit_field_value(const volatile uint32_t& value) {
+  static_assert(NUM_BITS + BIT_FIELD_OFFSET <= 32,
+                "Invalid bit field parameters. The total number of bits (offset and the number "
+                "of bits in the field) must be less than or equal to the size of the register "
+                "which is 32 bits. Likely, this is a typo or other scrivener's error in the "
+                "template parameters to call this method.");
+  constexpr uint32_t MASK{compute_bit_mask<NUM_BITS, BIT_FIELD_OFFSET>()};
+  return (value & MASK) >> BIT_FIELD_OFFSET;
+}
+
+/**
+ * Set a particular set of bits in a value, leaving the others unchanged.
+ */
+template <uint8_t NUM_BITS, uint8_t BIT_FIELD_OFFSET>
+inline constexpr void modify_bit_field(uint32_t& value, uint32_t bit_field_value) {
+  static_assert(NUM_BITS + BIT_FIELD_OFFSET <= 32,
+                "Invalid bit field parameters. The total number of bits (offset and the number "
+                "of bits in the field) must be less than or equal to the size of the register "
+                "which is 32 bits. Likely, this is a typo or other scrivener's error in the "
+                "template parameters to call this method.");
+  constexpr uint32_t MASK{compute_bit_mask<NUM_BITS, BIT_FIELD_OFFSET>()};
+  value = ((bit_field_value << BIT_FIELD_OFFSET) & MASK) | (value & ~MASK);
+}
+
+/**
+ * Set a particular set of bits in a value, leaving the others unchanged.
+ */
+template <uint8_t NUM_BITS, uint8_t BIT_FIELD_OFFSET>
+inline void modify_bit_field(volatile uint32_t& value, uint32_t bit_field_value) {
+  static_assert(NUM_BITS + BIT_FIELD_OFFSET <= 32,
+                "Invalid bit field parameters. The total number of bits (offset and the number "
+                "of bits in the field) must be less than or equal to the size of the register "
+                "which is 32 bits. Likely, this is a typo or other scrivener's error in the "
+                "template parameters to call this method.");
+  constexpr uint32_t MASK{compute_bit_mask<NUM_BITS, BIT_FIELD_OFFSET>()};
+  value = ((bit_field_value << BIT_FIELD_OFFSET) & MASK) | (value & ~MASK);
+}
+
+/**
+ * Set a particular set of bits in a value, leaving the others unchanged.
+ */
+template <uint8_t NUM_BITS>
+inline constexpr void modify_bit_field(uint32_t& value, uint32_t bit_field_value,
+                                       uint8_t bit_field_offset) {
+  const uint32_t MASK{compute_bit_mask<NUM_BITS>(bit_field_offset)};
+  value = ((bit_field_value << bit_field_offset) & MASK) | (value & ~MASK);
+}
+
+/**
+ * Set a particular set of bits in a value, leaving the others unchanged.
+ */
+template <uint8_t NUM_BITS>
+inline void modify_bit_field(volatile uint32_t& value, uint32_t bit_field_value,
+                             uint8_t bit_field_offset) {
+  const uint32_t MASK{compute_bit_mask<NUM_BITS>(bit_field_offset)};
+  value = ((bit_field_value << bit_field_offset) & MASK) | (value & ~MASK);
+}
+
+/**
+ * Set a particular set of bits in a value, leaving the others unchanged.
+ */
+inline constexpr void modify_bit_field(uint32_t& value, uint32_t bit_field_value, uint8_t num_bits,
+                                       uint8_t bit_field_offset) {
+  const uint32_t MASK{compute_bit_mask(num_bits, bit_field_offset)};
+  value = ((bit_field_value << bit_field_offset) & MASK) | (value & ~MASK);
+}
+
+/**
+ * Set a particular set of bits in a value, leaving the others unchanged.
+ */
+inline void modify_bit_field(volatile uint32_t& value, uint32_t bit_field_value, uint8_t num_bits,
+                             uint8_t bit_field_offset) {
+  const uint32_t MASK{compute_bit_mask(num_bits, bit_field_offset)};
+  uint32_t result = value;
+  value = ((bit_field_value << bit_field_offset) & MASK) | (result & ~MASK);
 }
 
 }  // namespace tvsc
--- base/bits_test.cc
+++ base/bits_test.cc
@@ -23,4 +23,103 @@ TEST(BitWidthTest, VerifyAssortedValues) {
   EXPECT_EQ(11, bit_width(1024));
 }
 
+template <uint8_t NUM_BITS, uint8_t BIT_FIELD_OFFSET>
+::testing::AssertionResult CanComputeBitMask(uint32_t expected_value) {
+  static constexpr uint32_t compile_time_value{compute_bit_mask<NUM_BITS, BIT_FIELD_OFFSET>()};
+  const uint32_t partial_compile_time_value{compute_bit_mask<NUM_BITS>(BIT_FIELD_OFFSET)};
+  const uint32_t run_time_value{compute_bit_mask(NUM_BITS, BIT_FIELD_OFFSET)};
+
+  if (expected_value != compile_time_value) {
+    return ::testing::AssertionFailure();
+  } else if (expected_value != partial_compile_time_value) {
+    return ::testing::AssertionFailure();
+  } else if (expected_value != run_time_value) {
+    return ::testing::AssertionFailure();
+  } else {
+    return ::testing::AssertionSuccess();
+  }
+}
+
+TEST(BitMaskTest, CanComputeBitMasks) {
+  ::testing::AssertionResult result{::testing::AssertionSuccess()};
+  result = CanComputeBitMask<2, 4>(0b00110000);
+  EXPECT_TRUE(result);
+  result = CanComputeBitMask<1, 4>(0b00010000);
+  EXPECT_TRUE(result);
+  result = CanComputeBitMask<17, 4>(0b00000000000111111111111111110000);
+  EXPECT_TRUE(result);
+  result = CanComputeBitMask<4, 17>(0b00000000000111100000000000000000);
+  EXPECT_TRUE(result);
+  result = CanComputeBitMask<5, 17>(0b00000000001111100000000000000000);
+  EXPECT_TRUE(result);
+}
+
+template <uint8_t NUM_BITS, uint8_t BIT_FIELD_OFFSET>
+::testing::AssertionResult CanModifyBitField() {
+  static constexpr uint32_t ZEROS{0x00000000};
+  static constexpr uint32_t ONES{0xffffffff};
+  static constexpr uint32_t EVENS{0xaaaaaaaa};
+  static constexpr uint32_t ODDS{0x55555555};
+  static constexpr uint32_t RANDOM{0b1010'0011'1101'1001'1101'1010'1011'0011};
+
+  static_assert(ONES == EVENS + ODDS,
+                "Bug in either EVENS or ODDS. They aren't bit patterns we expect.");
+
+  uint32_t zeros_modified{ZEROS};
+  uint32_t ones_modified{ONES};
+  uint32_t evens_modified{EVENS};
+  uint32_t odds_modified{ODDS};
+  uint32_t random_modified{RANDOM};
+
+  for (int i = 0; i < 5; ++i) {
+    uint32_t temp{zeros_modified};
+    modify_bit_field<NUM_BITS, BIT_FIELD_OFFSET>(
+        zeros_modified, get_bit_field_value<NUM_BITS, BIT_FIELD_OFFSET>(random_modified));
+    modify_bit_field<NUM_BITS, BIT_FIELD_OFFSET>(
+        random_modified, get_bit_field_value<NUM_BITS, BIT_FIELD_OFFSET>(odds_modified));
+    modify_bit_field<NUM_BITS, BIT_FIELD_OFFSET>(
+        odds_modified, get_bit_field_value<NUM_BITS, BIT_FIELD_OFFSET>(evens_modified));
+    modify_bit_field<NUM_BITS, BIT_FIELD_OFFSET>(
+        evens_modified, get_bit_field_value<NUM_BITS, BIT_FIELD_OFFSET>(ones_modified));
+    modify_bit_field<NUM_BITS, BIT_FIELD_OFFSET>(
+        ones_modified, get_bit_field_value<NUM_BITS, BIT_FIELD_OFFSET>(temp));
+  }
+
+  if (zeros_modified != ZEROS) {
+    return ::testing::AssertionFailure();
+  } else if (ones_modified != ONES) {
+    return ::testing::AssertionFailure();
+  } else if (evens_modified != EVENS) {
+    return ::testing::AssertionFailure();
+  } else if (odds_modified != ODDS) {
+    return ::testing::AssertionFailure();
+  } else if (random_modified != RANDOM) {
+    return ::testing::AssertionFailure();
+  } else {
+    return ::testing::AssertionSuccess();
+  }
+}
+
+TEST(BitFieldTest, CanModifyBitFields) {
+  ::testing::AssertionResult result{::testing::AssertionSuccess()};
+  result = CanModifyBitField<1, 0>();
+  EXPECT_TRUE(result);
+  result = CanModifyBitField<2, 0>();
+  EXPECT_TRUE(result);
+  result = CanModifyBitField<1, 2>();
+  EXPECT_TRUE(result);
+  result = CanModifyBitField<1, 3>();
+  EXPECT_TRUE(result);
+  result = CanModifyBitField<6, 12>();
+  EXPECT_TRUE(result);
+  result = CanModifyBitField<11, 13>();
+  EXPECT_TRUE(result);
+  result = CanModifyBitField<13, 17>();
+  EXPECT_TRUE(result);
+  result = CanModifyBitField<13, 19>();
+  EXPECT_TRUE(result);
+  result = CanModifyBitField<3, 27>();
+  EXPECT_TRUE(result);
+}
+
 }  // namespace tvsc
