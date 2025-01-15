8e946817a160aa97af88d383eaad341b871ef6e6
James Pack <jgrimm@gmail.com>
2025-01-15 12:06:57 -0500

    Demo using all 3 LEDs

--- hal/boards/nucleo_h743zi/blink.cc
+++ hal/boards/nucleo_h743zi/blink.cc
@@ -1,3 +1,5 @@
+#include <cstring>
+
 #include "hal/boards/nucleo_h743zi/board_nucleo_h743zi.h"
 #include "hal/gpio/gpio.h"
 #include "hal/gpio/pins.h"
@@ -9,11 +11,6 @@ using namespace tvsc::hal::gpio;
 int main() {
   BoardType board{};
 
-  // Configure the push button as an input.
-  board.gpio<BoardType::BLUE_PUSH_BUTTON_PORT>().set_pin_mode(BoardType::BLUE_PUSH_BUTTON_PIN,
-                                                              PinMode::INPUT_FLOATING);
-
-  /* Configure the debug LEDs. */
   board.gpio<BoardType::RED_LED_PORT>().set_pin_mode(BoardType::RED_LED_PIN,
                                                      PinMode::OUTPUT_PUSH_PULL, PinSpeed::LOW);
   board.gpio<BoardType::YELLOW_LED_PORT>().set_pin_mode(BoardType::YELLOW_LED_PIN,
@@ -22,6 +19,22 @@ int main() {
                                                        PinMode::OUTPUT_PUSH_PULL, PinSpeed::LOW);
 
   while (true) {
+    board.gpio<BoardType::RED_LED_PORT>().toggle_pin(BoardType::RED_LED_PIN);
+    board.gpio<BoardType::YELLOW_LED_PORT>().toggle_pin(BoardType::YELLOW_LED_PIN);
+
+    board.clock().sleep_ms(750);
+
+    board.gpio<BoardType::YELLOW_LED_PORT>().toggle_pin(BoardType::YELLOW_LED_PIN);
+    board.gpio<BoardType::GREEN_LED_PORT>().toggle_pin(BoardType::GREEN_LED_PIN);
+
+    board.clock().sleep_ms(750);
+
+    board.gpio<BoardType::RED_LED_PORT>().toggle_pin(BoardType::RED_LED_PIN);
+    board.gpio<BoardType::YELLOW_LED_PORT>().toggle_pin(BoardType::YELLOW_LED_PIN);
+
+    board.clock().sleep_ms(750);
+
+    board.gpio<BoardType::YELLOW_LED_PORT>().toggle_pin(BoardType::YELLOW_LED_PIN);
     board.gpio<BoardType::GREEN_LED_PORT>().toggle_pin(BoardType::GREEN_LED_PIN);
 
     board.clock().sleep_ms(750);
