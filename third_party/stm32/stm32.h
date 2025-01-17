938518bcdd9996368881f0ad448560694c8bf00d
James Pack <jgrimm@gmail.com>
2025-01-17 11:31:03 -0500

    Add more MCU targets in stm32_libraries. Mark the specific library targets as corresponding to the MCU, not the series of processors.

--- third_party/stm32/stm32.h
+++ third_party/stm32/stm32.h
@@ -6,11 +6,15 @@ extern "C" {
 
 #if defined(STM32H743xx)
 #include "stm32h743xx.h"
+#elif defined(STM32L412xx)
+#include "stm32l412xx.h"
+#elif defined(STM32L432xx)
+#include "stm32l432xx.h"
 #elif defined(STM32L452xx)
 #include "stm32l452xx.h"
 #else
 #error \
-    "Please configure toolchain to define the appropriate symbol (STM32H743xx, STM32L452xx, etc.) for the platform"
+    "Please configure this file to include the appropriate symbol (STM32H743xx, STM32L452xx, etc.) for the platform. Alternatively, update .bazelrc to define the appropriate symbol using --copt."
 #endif
 
 #ifdef __cplusplus
