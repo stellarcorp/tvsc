f8b5643f06d2874065f4c9d9a343a7d1d5cfb8d8
James Pack <jgrimm@gmail.com>
2025-01-17 06:03:50 -0500

    Made clock class name generic across STM32 processors

--- hal/time/stm_clock.cc
+++ hal/time/stm_clock.cc
@@ -4,16 +4,16 @@
 
 namespace tvsc::hal::time {
 
-time::TimeType ClockStm32H7xx::current_time_micros() { return *current_time_us_; }
-time::TimeType ClockStm32H7xx::current_time_millis() { return *current_time_us_ / 1000; }
+time::TimeType ClockStm32xxxx::current_time_micros() { return *current_time_us_; }
+time::TimeType ClockStm32xxxx::current_time_millis() { return *current_time_us_ / 1000; }
 
-void ClockStm32H7xx::sleep_us(time::TimeType microseconds) {
+void ClockStm32xxxx::sleep_us(time::TimeType microseconds) {
   const time::TimeType sleep_until_us{current_time_micros() + microseconds};
   while (current_time_micros() < sleep_until_us) {
     // Do nothing
   }
 }
 
-void ClockStm32H7xx::sleep_ms(time::TimeType milliseconds) { sleep_us(milliseconds * 1000); }
+void ClockStm32xxxx::sleep_ms(time::TimeType milliseconds) { sleep_us(milliseconds * 1000); }
 
 }  // namespace tvsc::hal::time
--- hal/time/stm_clock.h
+++ hal/time/stm_clock.h
@@ -7,12 +7,12 @@ namespace tvsc::hal::time {
 /**
  * Class to manage the compute the current time and handle sleep requests for the STM32H7xx.
  */
-class ClockStm32H7xx final : public time::Clock {
+class ClockStm32xxxx final : public time::Clock {
  private:
   const volatile CTimeType* const current_time_us_;
 
  public:
-  ClockStm32H7xx(const volatile CTimeType* current_time_us) : current_time_us_(current_time_us) {}
+  ClockStm32xxxx(const volatile CTimeType* current_time_us) : current_time_us_(current_time_us) {}
 
   time::TimeType current_time_micros() override;
   time::TimeType current_time_millis() override;
