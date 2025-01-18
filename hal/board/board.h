e6ecc02abc04ec3035675e5659bf2e844c7e7bf6
James Pack <jgrimm@gmail.com>
2025-01-18 12:47:08 -0500

    Modify board selection to use the name of the board, rather than the processor series.

--- hal/board/board.h
+++ hal/board/board.h
@@ -1,14 +1,14 @@
 #pragma once
 
-#if defined(STM32H743xx)
+#if defined(NUCLEO_H743ZI)
 #include "hal/board/nucleo_h743zi_board.h"
-#elif defined(STM32L412xx)
+#elif defined(NUCLEO_L412KB)
 #include "hal/board/nucleo_l412kb_board.h"
-#elif defined(STM32L432xx)
+#elif defined(NUCLEO_L432KC)
 #include "hal/board/nucleo_l432kc_board.h"
-#elif defined(STM32L452xx)
+#elif defined(NUCLEO_L452RE)
 #include "hal/board/nucleo_l452re_board.h"
 #else
 #error \
-    "Please configure this file to include the appropriate symbol (STM32H743xx, STM32L452xx, etc.) for the platform. Alternatively, update .bazelrc to define the appropriate symbol using --copt."
+    "Please configure this file to include the appropriate symbol (NUCLEO_L412KB, etc.) for the platform. Alternatively, update .bazelrc to define the appropriate symbol using --copt."
 #endif
