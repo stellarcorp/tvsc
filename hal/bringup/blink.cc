4bc2293ab87f27a460cb80a77d4334c63e14c835
James Pack <jgrimm@gmail.com>
2025-01-21 06:01:29 -0500

    Extract bringup demos to their own package. Separate the power demo from the simple blink executable.

---
+++ hal/bringup/blink.cc
@@ -0,0 +1,27 @@
+#include <cstring>
+
+#include "hal/board/board.h"
+#include "hal/gpio/gpio.h"
+
+using BoardType = tvsc::hal::board::Board;
+
+using namespace tvsc::hal::gpio;
+
+int main() {
+  BoardType board{};
+
+  // Turn on clocks for the GPIO ports that we want.
+  board.rcc().enable_gpio_port(BoardType::GREEN_LED_PORT);
+
+  auto& gpio{board.gpio<BoardType::GREEN_LED_PORT>()};
+  gpio.set_pin_mode(BoardType::GREEN_LED_PIN, PinMode::OUTPUT_PUSH_PULL, PinSpeed::LOW);
+
+  while (true) {
+    auto& clock{board.clock()};
+
+    gpio.toggle_pin(BoardType::GREEN_LED_PIN);
+    clock.sleep_ms(500);
+    gpio.toggle_pin(BoardType::GREEN_LED_PIN);
+    clock.sleep_ms(500);
+  }
+}
