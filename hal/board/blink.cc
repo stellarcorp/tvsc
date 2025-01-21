9b48b96f1b971e5a0b05fbcae2925db445d8839b
James Pack <jgrimm@gmail.com>
2025-01-21 05:51:24 -0500

    Minor readability improvement

--- hal/board/blink.cc
+++ hal/board/blink.cc
@@ -9,41 +9,43 @@ using namespace tvsc::hal::gpio;
 
 int main() {
   BoardType board{};
+  auto& rcc{board.rcc()};
+  auto& power{board.power()};
 
   static constexpr bool demo_speed_changes{true};
   bool at_max_speed{false};
   if constexpr (demo_speed_changes) {
-    board.rcc().set_clock_to_min_speed();
-    board.power().enter_low_power_run_mode();
+    rcc.set_clock_to_min_speed();
+    power.enter_low_power_run_mode();
   }
 
   // Turn on clocks for the GPIO ports that we want.
-  board.rcc().enable_gpio_port(BoardType::GREEN_LED_PORT);
+  rcc.enable_gpio_port(BoardType::GREEN_LED_PORT);
 
-  board.gpio<BoardType::GREEN_LED_PORT>().set_pin_mode(BoardType::GREEN_LED_PIN,
-                                                       PinMode::OUTPUT_PUSH_PULL, PinSpeed::LOW);
+  auto& gpio{board.gpio<BoardType::GREEN_LED_PORT>()};
+
+  gpio.set_pin_mode(BoardType::GREEN_LED_PIN, PinMode::OUTPUT_PUSH_PULL, PinSpeed::LOW);
 
   while (true) {
     for (int i = 0; i < 2; ++i) {
-      board.gpio<BoardType::GREEN_LED_PORT>().toggle_pin(BoardType::GREEN_LED_PIN);
+      auto& clock{board.clock()};
 
-      board.clock().sleep_ms(500);
-
-      board.gpio<BoardType::GREEN_LED_PORT>().toggle_pin(BoardType::GREEN_LED_PIN);
-
-      board.clock().sleep_ms(500);
+      gpio.toggle_pin(BoardType::GREEN_LED_PIN);
+      clock.sleep_ms(500);
+      gpio.toggle_pin(BoardType::GREEN_LED_PIN);
+      clock.sleep_ms(500);
     }
 
     // Swap speeds so that we can detect any behavior changes due to the speed in the registers, LED
     // blinking, etc.
     if constexpr (demo_speed_changes) {
       if (at_max_speed) {
-        board.rcc().set_clock_to_min_speed();
-        board.power().enter_low_power_run_mode();
+        rcc.set_clock_to_min_speed();
+        power.enter_low_power_run_mode();
         at_max_speed = false;
       } else {
-        board.power().exit_low_power_run_mode();
-        board.rcc().set_clock_to_max_speed();
+        power.exit_low_power_run_mode();
+        rcc.set_clock_to_max_speed();
         at_max_speed = true;
       }
     }
