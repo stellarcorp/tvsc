beecd14e13e1d9e30c8cdd3241e4b34ccab1592c
James Pack <jgrimm@gmail.com>
2025-01-25 11:03:06 -0500

    Remove references to push buttons on Nucleo L432KC and Nucleo L412KB boards. Neither of these boards have a programmable push button. The push buttons on these boards are tied to NRST and perform a reset of the board.

--- hal/board/nucleo_l412kb_board.h
+++ hal/board/nucleo_l412kb_board.h
@@ -34,10 +34,6 @@ class Board final {
   static constexpr gpio::Port RED_LED_PORT{GPIO_PORT_B};
   static constexpr gpio::Pin RED_LED_PIN{14};
 
-  // Location of the push button on this board.
-  static constexpr gpio::Port BLUE_PUSH_BUTTON_PORT{GPIO_PORT_C};
-  static constexpr gpio::Pin BLUE_PUSH_BUTTON_PIN{13};
-
  private:
   rcc::RccStm32L4xx rcc_{reinterpret_cast<void*>(RCC_BASE), reinterpret_cast<void*>(SysTick_BASE),
                          reinterpret_cast<void*>(ADC1_BASE)};
--- hal/board/nucleo_l432kc_board.h
+++ hal/board/nucleo_l432kc_board.h
@@ -36,10 +36,6 @@ class Board final {
   static constexpr gpio::Port RED_LED_PORT{GPIO_PORT_B};
   static constexpr gpio::Pin RED_LED_PIN{14};
 
-  // Location of the push button on this board.
-  static constexpr gpio::Port BLUE_PUSH_BUTTON_PORT{GPIO_PORT_C};
-  static constexpr gpio::Pin BLUE_PUSH_BUTTON_PIN{13};
-
   static constexpr gpio::Port DAC_PORT{GPIO_PORT_A};
   static constexpr gpio::Pin DAC_PIN{4};
 
