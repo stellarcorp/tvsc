0106a82bac99e32cebcad574a1a98fca29bd2576
James Pack <jgrimm@gmail.com>
2025-01-28 21:35:58 -0500

    Reimplement blink using coroutines

--- hal/bringup/blink.cc
+++ hal/bringup/blink.cc
@@ -2,10 +2,23 @@
 
 #include "hal/board/board.h"
 #include "hal/gpio/gpio.h"
+#include "hal/scheduler/scheduler.h"
+#include "hal/scheduler/task.h"
+#include "hal/time/clock.h"
 
 using BoardType = tvsc::hal::board::Board;
 
 using namespace tvsc::hal::gpio;
+using namespace tvsc::hal::scheduler;
+using namespace tvsc::hal::time;
+
+template <uint64_t DELAY_MS = 500>
+Task blink(Clock& clock, Gpio& gpio, Pin pin) {
+  while (true) {
+    gpio.toggle_pin(pin);
+    co_yield(clock.current_time_millis() + DELAY_MS) * 1000;
+  }
+}
 
 int main() {
   BoardType board{};
@@ -14,14 +27,11 @@ int main() {
   board.rcc().enable_gpio_port_clock(BoardType::GREEN_LED_PORT);
 
   auto& gpio{board.gpio<BoardType::GREEN_LED_PORT>()};
-  gpio.set_pin_mode(BoardType::GREEN_LED_PIN, PinMode::OUTPUT_PUSH_PULL, PinSpeed::LOW);
+  gpio.set_pin_mode(BoardType::GREEN_LED_PIN, PinMode::OUTPUT_PUSH_PULL);
+  auto& clock{board.clock()};
 
-  while (true) {
-    auto& clock{board.clock()};
-
-    gpio.toggle_pin(BoardType::GREEN_LED_PIN);
-    clock.sleep_ms(500);
-    gpio.toggle_pin(BoardType::GREEN_LED_PIN);
-    clock.sleep_ms(500);
-  }
+  Scheduler<4> scheduler{clock};
+  Task task{blink(clock, gpio, BoardType::GREEN_LED_PIN)};
+  scheduler.add_task(task);
+  scheduler.start();
 }
