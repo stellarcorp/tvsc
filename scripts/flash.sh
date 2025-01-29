e5a40f22af3027949c107773d4889a1ffdc032cc
James Pack <jgrimm@gmail.com>
2025-01-29 13:52:45 -0500

    Update flash.sh script to select which board to flash when multiple boards are connected

--- scripts/flash.sh
+++ scripts/flash.sh
@@ -1,3 +1,22 @@
 #!/usr/bin/env bash
 
-/opt/st/stm32cube_programmer/bin/STM32_Programmer.sh --connect port=SWD shared --write "$1" --verify -rst -run
+INDEX="0"
+
+remaining_args=()
+
+#Loop through arguments
+while [[ $# -gt 0 ]]
+do
+  case "$1" in
+    --index=*)
+	INDEX="${1#*=}"  # Extract value after '='
+	shift
+      ;;
+    *)
+      remaining_args+=("$1")  # Store unknown arguments
+      shift                   # Remove current argument
+      ;;
+  esac
+done
+
+/opt/st/stm32cube_programmer/bin/STM32_Programmer.sh --connect port=SWD index=${INDEX} shared --write "${remaining_args[@]}" --verify -rst -run
