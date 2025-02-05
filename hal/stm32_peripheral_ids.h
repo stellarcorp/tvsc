f85475535bae3d8a9b84cdd6350248dab97ca145
James Pack <jgrimm@gmail.com>
2025-02-05 12:00:51 -0500

    Added more peripheral ids for the STM32 series

--- hal/stm32_peripheral_ids.h
+++ hal/stm32_peripheral_ids.h
@@ -6,19 +6,31 @@ namespace tvsc::hal {
 
 /**
  * Numerical ids identifying each of the peripherals in STM32 MCUs. The symbols in the enum attempt
- * to use the same names as in the code and literature from ST Micro. This list is not exhaustive;
- * we add ids when they are needed. Also, not all peripherals identified here are available in every
- * board. We could make enums for every board. That would allow us to catch more errors at
- * compile-time. But, so far, it hasn't been worth the extra design and maintenance effort.
+ * to use the same names as in the code and literature from ST Micro. We add the suffix of ID, since
+ * ST defines the corresponding symbol as a macro, defeating any namespacing. This list is not
+ * exhaustive; we add ids when they are needed. Also, not all peripherals identified here are
+ * available in every board. We could make enums for every board. That would allow us to catch more
+ * errors at compile-time. But, so far, it hasn't been worth the extra design and maintenance
+ * effort.
  */
 class Stm32PeripheralIds final {
  public:
   // Timers
-  static constexpr PeripheralId TIM1{1};
-  static constexpr PeripheralId TIM2{2};
-  static constexpr PeripheralId TIM3{3};
-  static constexpr PeripheralId TIM15{4};
-  static constexpr PeripheralId LPTIM1{5};
+  static constexpr PeripheralId TIM1_ID{1};
+  static constexpr PeripheralId TIM2_ID{2};
+  static constexpr PeripheralId TIM3_ID{3};
+  static constexpr PeripheralId TIM4_ID{4};
+  static constexpr PeripheralId TIM6_ID{5};
+  static constexpr PeripheralId TIM8_ID{6};
+  static constexpr PeripheralId TIM15_ID{7};
+  static constexpr PeripheralId LPTIM1_ID{8};
+
+  // DMAs
+  // Note that DMAs are mostly identified by channel, rather than the peripheral itself. In ST
+  // Micro's HAL, the main configuration structure DMA_Handle_TypeDef has an instance of the channel
+  // (DMA_Channel_TypeDef). So, we follow the same pattern and give ids for the channels in each DMA
+  // peripheral.
+  static constexpr PeripheralId DMA1_CHANNEL1_ID{20};
 
   // RCC
 
@@ -29,8 +41,6 @@ class Stm32PeripheralIds final {
   // SPI buses
 
   // CAN buses
-
-  // DMAs
 
   // ADCs
 
