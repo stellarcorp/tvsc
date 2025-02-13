08df5050498e2d95c6f96d94d58a7f215332a978
James Pack <jgrimm@gmail.com>
2025-02-13 16:15:32 -0500

    Added support for random number generation over real number ranges.

--- random/random.h
+++ random/random.h
@@ -3,6 +3,7 @@
 #include <cstdint>
 #include <limits>
 #include <random>
+#include <type_traits>
 
 namespace tvsc::random {
 
@@ -10,10 +11,17 @@ std::default_random_engine& engine();
 
 inline void set_seed(uint32_t seed) { engine().seed(seed); }
 
-template <typename ResultT>
+template <typename ResultT, std::enable_if_t<std::is_integral<ResultT>::value, bool> = true>
 inline ResultT generate_random_value(ResultT minimum = std::numeric_limits<ResultT>::lowest(),
                                      ResultT maximum = std::numeric_limits<ResultT>::max()) {
   std::uniform_int_distribution dist{minimum, maximum};
+  return dist(engine());
+}
+
+template <typename ResultT, std::enable_if_t<std::is_floating_point<ResultT>::value, bool> = true>
+inline ResultT generate_random_value(ResultT minimum = std::numeric_limits<ResultT>::lowest(),
+                                     ResultT maximum = std::numeric_limits<ResultT>::max()) {
+  std::uniform_real_distribution dist{minimum, maximum};
   return dist(engine());
 }
 
