d4dcdd1a8e125f886324a87ed2671971c9d347a0
James Pack <jgrimm@gmail.com>
2025-02-17 02:05:24 -0500

    Use new Peripheral and Functional base classes in the DAC peripheral

--- hal/dac/dac.cc
+++
@@ -1,7 +0,0 @@
-#include "hal/dac/dac.h"
-
-namespace tvsc::hal::dac {
-
-Dac DacPeripheral::access() { return Dac(*this); }
-
-}  // namespace tvsc::hal::dac
--- hal/dac/dac.h
+++ hal/dac/dac.h
@@ -4,27 +4,14 @@
 #include <cstdint>
 #include <utility>
 
+#include "hal/peripheral.h"
+
 namespace tvsc::hal::dac {
 
 class Dac;
 
-class DacPeripheral {
+class DacPeripheral : public Peripheral<DacPeripheral, Dac> {
  private:
-  size_t ref_count_{};
-
- protected:
-  void inc_ref_count() {
-    if (ref_count_++ == 0) {
-      enable();
-    }
-  }
-
-  void dec_ref_count() {
-    if (--ref_count_ == 0) {
-      disable();
-    }
-  }
-
   virtual void enable() = 0;
   virtual void disable() = 0;
 
@@ -37,52 +24,21 @@ class DacPeripheral {
 
  public:
   virtual ~DacPeripheral() = default;
-
-  // Use the functionality of this peripheral. Power and clock will be enabled.
-  Dac access();
 };
 
-class Dac final {
+class Dac final : public Functional<DacPeripheral, Dac> {
  private:
-  DacPeripheral* peripheral_;
+  explicit Dac(DacPeripheral& peripheral) : Functional<DacPeripheral, Dac>(peripheral) {}
 
-  explicit Dac(DacPeripheral& peripheral) : peripheral_(&peripheral) {
-    peripheral_->inc_ref_count();
-  }
-
-  friend class DacPeripheral;
+  friend class Peripheral<DacPeripheral, Dac>;
 
  public:
-  ~Dac() {
-    if (peripheral_ != nullptr) {
-      peripheral_->dec_ref_count();
-    }
-  }
-
-  Dac(Dac&& rhs) : peripheral_(std::exchange(rhs.peripheral_, nullptr)) {}
-
-  Dac& operator=(Dac&& rhs) {
-    std::swap(peripheral_, rhs.peripheral_);
-    return *this;
-  }
-
-  Dac(const Dac& rhs) : peripheral_(rhs.peripheral_) { peripheral_->inc_ref_count(); }
-
-  Dac& operator=(const Dac& rhs) {
-    peripheral_ = rhs.peripheral_;
-    peripheral_->inc_ref_count();
-    return *this;
-  }
-
   void set_value(uint32_t value, uint8_t channel = 0) { peripheral_->set_value(value, channel); }
   void clear_value(uint8_t channel = 0) { peripheral_->clear_value(channel); }
 
   void set_resolution(uint8_t bits_resolution, uint8_t channel = 0) {
     peripheral_->set_resolution(bits_resolution, channel);
   }
-
-  // Turn on power and clock to this peripheral.
-  Dac access();
 };
 
 }  // namespace tvsc::hal::dac
