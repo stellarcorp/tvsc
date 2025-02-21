80afc162323a8d62e31d9b944b141b53bedae4d8
James Pack <jgrimm@gmail.com>
2025-02-21 04:55:46 -0500

    Cleanup generate_random_value() methods to return the std::common_type between the two arguments.

--- random/random.h
+++ random/random.h
@@ -16,9 +16,8 @@ template <typename FirstT, typename SecondT = FirstT,
                            bool> = true>
 inline auto generate_random_value(FirstT minimum = std::numeric_limits<FirstT>::lowest(),
                                   SecondT maximum = std::numeric_limits<SecondT>::max())
-    // TODO(james): Switch to using std::common_type here and similar below.
-    -> decltype(minimum + maximum) {
-  using ResultT = decltype(minimum + maximum);
+    -> std::common_type_t<FirstT, SecondT> {
+  using ResultT = std::common_type_t<FirstT, SecondT>;
   std::uniform_int_distribution dist{static_cast<ResultT>(minimum), static_cast<ResultT>(maximum)};
   return dist(engine());
 }
@@ -29,8 +28,8 @@ template <typename FirstT, typename SecondT = FirstT,
                            bool> = true>
 inline auto generate_random_value(FirstT minimum = std::numeric_limits<FirstT>::lowest(),
                                   SecondT maximum = std::numeric_limits<SecondT>::max())
-    -> decltype(minimum + maximum) {
-  using ResultT = decltype(minimum + maximum);
+    -> std::common_type_t<FirstT, SecondT> {
+  using ResultT = std::common_type_t<FirstT, SecondT>;
   std::uniform_real_distribution dist{static_cast<ResultT>(minimum), static_cast<ResultT>(maximum)};
   return dist(engine());
 }
