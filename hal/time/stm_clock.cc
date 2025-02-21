a4ce0f9829f45bebb12df4d1eb66f3339a8e4d4d
James Pack <jgrimm@gmail.com>
2025-02-21 03:18:35 -0500

    Documented logic in sleep_us() on when we enter stop mode and when we don't.

--- hal/time/stm_clock.cc
+++ hal/time/stm_clock.cc
@@ -20,13 +20,15 @@ TimeType ClockStm32xxxx::current_time_micros() { return uwTick * 1000; }
 TimeType ClockStm32xxxx::current_time_millis() { return uwTick; }
 
 void ClockStm32xxxx::sleep_us(TimeType microseconds) {
+  static constexpr TimeType TIME_TO_WAKE_FROM_STOP_MODE_US{500};
   // Start the timer. Assume that it will trigger an interrupt at the end of the interval. Then
   // enter stop mode. We exit stop mode on any interrupt (or possibly any EXTI event as well). So,
   // we wrap the call to enter stop mode with a check to see if the timer is still running; when it
-  // is not running, its interrupt has fired. Alternatively, for short sleeps, we simply block on
-  // WFI (Wait For Interrupt).
+  // is not running, its interrupt has fired. Alternatively, for short sleeps where the sleep time
+  // is less than the time it takes to wake from stop mode, we simply block on WFI (Wait For
+  // Interrupt). Note: entering stop mode is only 1-2 clock cycles, so we ignore that time.
   timer_.start(microseconds);
-  if (microseconds < 500) {
+  if (microseconds < TIME_TO_WAKE_FROM_STOP_MODE_US) {
     while (timer_.is_running()) {
       __WFI();
     }
