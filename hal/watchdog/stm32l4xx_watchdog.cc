10468cfc599dc6b38ec17d1aef85f89f1c46c39b
James Pack <jgrimm@gmail.com>
2025-02-22 08:14:38 -0500

    Add protections to ensure that the IWDG Reload value is in the correct units.

--- hal/watchdog/stm32l4xx_watchdog.cc
+++ hal/watchdog/stm32l4xx_watchdog.cc
@@ -12,14 +12,18 @@ void WatchdogStm32l4xx::feed() { HAL_IWDG_Refresh(&watchdog_); }
 
 std::chrono::milliseconds WatchdogStm32l4xx::reset_interval() {
   using namespace std::chrono_literals;
-  return 1000ms;
+  return 1s;
 }
 
 void WatchdogStm32l4xx::enable() {
   lsi_active_ = oscillator_->access();
 
   watchdog_.Init.Prescaler = IWDG_PRESCALER_32;  // LSI / 32 = 1 kHz
-  watchdog_.Init.Reload = reset_interval().count();
+  // Ensure that the reset_interval() is expressed in milliseconds before we call the count()
+  // method. The prescaler above puts the IWDG counting in milliseconds. If the reset_interval is in
+  // a different unit, we will have a mismatch.
+  const auto interval{std::chrono::duration_cast<std::chrono::milliseconds>(reset_interval())};
+  watchdog_.Init.Reload = interval.count();
   watchdog_.Init.Window = watchdog_.Init.Reload;
   HAL_IWDG_Init(&watchdog_);
 }
