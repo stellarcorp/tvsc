55494aa9f73555b89faa06d6519091d7326a0a51
James Pack <jgrimm@gmail.com>
2025-02-22 08:15:52 -0500

    Express the reset parameter using std::chrono::duration instead of using an integer counter.

--- hal/bringup/watchdog_demo.cc
+++ hal/bringup/watchdog_demo.cc
@@ -21,16 +21,23 @@ using BoardType = tvsc::hal::board::Board;
 using ClockType = BoardType::ClockType;
 using TaskType = tvsc::hal::scheduler::Task<ClockType>;
 
-TaskType run_watchdog(watchdog::WatchdogPeripheral& watchdog_peripheral,
-                      uint32_t reset_count = std::numeric_limits<uint32_t>::max()) {
-  const auto feed_interval{watchdog_peripheral.reset_interval() / 4};
+using namespace std::chrono_literals;
+
+template <typename Duration = std::chrono::years>
+TaskType run_watchdog(ClockType& clock, watchdog::WatchdogPeripheral& watchdog_peripheral,
+                      Duration reset_in = std::chrono::duration_cast<Duration>(1y)) {
+  const auto feed_interval{watchdog_peripheral.reset_interval() / 2};
+  const auto reset_at{clock.current_time() + reset_in};
 
   // Enable the watchdog here. After this, if the dog isn't fed on time, the board will reset.
   auto watchdog{watchdog_peripheral.access()};
-  while (watchdog_counter < reset_count) {
+  while (true) {
     co_yield feed_interval;
     watchdog.feed();
     ++watchdog_counter;
+    if (clock.current_time() > reset_at) {
+      break;
+    }
   }
 
   // Stop feeding, but keep the watchdog instance alive. This will trigger a reset.
@@ -46,15 +53,16 @@ using namespace tvsc::hal::bringup;
 using namespace tvsc::hal::scheduler;
 
 int main() {
+  static constexpr auto CYCLE_TIME{5s};
   BoardType& board{BoardType::board()};
   auto& clock{board.clock()};
 
   // Sleep now so that we can detect the board reset. If we just launch into the tasks, we might not
   // be able to detect that the watchdog caused a reset.
-  clock.sleep_ms(5'000);
+  clock.sleep(CYCLE_TIME);
 
   Scheduler<ClockType, 4 /*QUEUE_SIZE*/> scheduler{clock, board.rcc()};
-  scheduler.add_task(run_watchdog(board.iwdg(), 20));
+  scheduler.add_task(run_watchdog(clock, board.iwdg(), CYCLE_TIME));
   scheduler.add_task(
       blink(clock, board.gpio<BoardType::GREEN_LED_PORT>(), BoardType::GREEN_LED_PIN));
   scheduler.start();
