fbfc727a767607319e645cc410dfeebed3925710
James Pack <jgrimm@gmail.com>
2025-03-06 10:38:38 -0500

    Clang-format.

--- bringup/random_blink.h
+++ bringup/random_blink.h
@@ -4,16 +4,16 @@
 
 #include "hal/board/board.h"
 #include "hal/gpio/gpio.h"
+#include "random/random.h"
 #include "scheduler/scheduler.h"
 #include "scheduler/task.h"
 #include "time/clock.h"
-#include "random/random.h"
 
 namespace tvsc::bringup {
 
 template <typename ClockType>
-tvsc::scheduler::Task<ClockType> blink_randomly(
-    tvsc::hal::gpio::GpioPeripheral& gpio_peripheral, tvsc::hal::gpio::Pin pin) {
+tvsc::scheduler::Task<ClockType> blink_randomly(tvsc::hal::gpio::GpioPeripheral& gpio_peripheral,
+                                                tvsc::hal::gpio::Pin pin) {
   auto gpio{gpio_peripheral.access()};
   gpio.set_pin_mode(pin, tvsc::hal::gpio::PinMode::OUTPUT_PUSH_PULL);
   gpio.write_pin(pin, 0);
