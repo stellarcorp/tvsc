0d297acad692ef67a9ec88325030a76caee6cde8
James Pack <jgrimm@gmail.com>
2025-03-06 13:39:36 -0500

    Add simple blink bringup script with minimal dependencies.

---
+++ bringup/systick_blink_demo.cc
@@ -0,0 +1,33 @@
+#include <chrono>
+
+#include "hal/board/board.h"
+#include "hal/gpio/gpio.h"
+#include "time/embedded_clock.h"
+
+using BoardType = tvsc::hal::board::Board;
+using ClockType = tvsc::time::EmbeddedClock;
+
+using namespace std::chrono_literals;
+
+int main() {
+  auto& board{BoardType::board()};
+  auto& clock{ClockType::clock()};
+
+  const auto delay{500ms};
+
+  tvsc::hal::gpio::GpioPeripheral& gpio_peripheral{board.gpio<BoardType::GREEN_LED_PORT>()};
+  tvsc::hal::gpio::Gpio gpio{gpio_peripheral.access()};
+  tvsc::hal::gpio::Pin pin{BoardType::GREEN_LED_PIN};
+
+  gpio.set_pin_mode(pin, tvsc::hal::gpio::PinMode::OUTPUT_PUSH_PULL);
+
+  while (true) {
+    gpio.toggle_pin(pin);
+
+    const auto change_time{clock.current_time() + delay};
+    while (clock.current_time() < change_time) {
+      // Busy loop to keep dependencies simple. This avoids using the scheduler and avoids
+      // sleep/stop modes used when using the sleep methods of the clock.
+    }
+  }
+}
