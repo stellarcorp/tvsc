7caa56c31535e308857145e3450ab7b6b0ebec9c
James Pack <jgrimm@gmail.com>
2025-03-06 10:50:49 -0500

    Add FakeSysTick for managing the SysTick service via simulated clock ticks.

---
+++ hal/systick/fake_systick.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "hal/simulation/event_generator.h"
+#include "hal/simulation/reactor.h"
+#include "hal/systick/systick.h"
+#include "hal/time_type.h"
+
+namespace tvsc::hal::systick {
+
+template <typename ClockT>
+class FakeSysTick final : public SysTickType, public simulation::EventGenerator<ClockT> {
+ public:
+  using ClockType = ClockT;
+  using ReactorType = simulation::Reactor<ClockType>;
+
+ private:
+  ReactorType* reactor_;
+  TimeType uwTick_{};
+
+ public:
+  FakeSysTick(ReactorType& reactor) : reactor_(&reactor) {}
+
+  TimeType current_time_micros() override { return uwTick_; }
+  void increment_micros(TimeType us) override { uwTick_ += us; }
+  void handle_interrupt() override { uwTick_ += 1000; }
+
+  ClockType::duration next_event_in(ClockType::time_point now) const noexcept override {
+    using namespace std::chrono_literals;
+    return 1000us;
+  }
+
+  void generate(ClockType::time_point now) noexcept override { handle_interrupt(); }
+};
+
+}  // namespace tvsc::hal::systick
