49c5f9f84dbfdbddbb4d10c994f9a43fe04338b5
James Pack <jgrimm@gmail.com>
2025-03-09 08:33:59 -0400

    Instrument with profiling and use a flag to determine when the simulation should stop.

--- bringup/systick_blink_demo.cc
+++ bringup/systick_blink_demo.cc
@@ -1,20 +1,30 @@
 #include <chrono>
+#include <cstdint>
+#include <limits>
 
 #include "base/initializer.h"
+#include "flags/flags.h"
 #include "hal/board/board.h"
 #include "hal/gpio/gpio.h"
+#include "profile/profile.h"
 #include "time/embedded_clock.h"
 
 using BoardType = tvsc::hal::board::Board;
 using ClockType = tvsc::time::EmbeddedClock;
+
+DEFINE_uint64(run_duration_sec, std::numeric_limits<uint64_t>::max(),
+              "How long to run this script in seconds");
 
 using namespace std::chrono_literals;
 
 int main(int argc, char* argv[]) {
   tvsc::initialize(&argc, &argv);
 
+  tvsc::profile::profiler_start();
+
   auto& board{BoardType::board()};
   auto& clock{ClockType::clock()};
+  const auto completion_time{clock.current_time() + std::chrono::seconds{FLAGS_run_duration_sec}};
 
   const auto delay{500ms};
 
@@ -24,7 +34,7 @@ int main(int argc, char* argv[]) {
 
   gpio.set_pin_mode(pin, tvsc::hal::gpio::PinMode::OUTPUT_PUSH_PULL);
 
-  while (true) {
+  while (clock.current_time() < completion_time) {
     gpio.toggle_pin(pin);
 
     const auto change_time{clock.current_time() + delay};
@@ -33,8 +43,12 @@ int main(int argc, char* argv[]) {
       // sleep/stop modes used when using the sleep methods of the clock.
       for (int i = 0; i < 1000; ++i) {
         // We use this inside for loop to avoid calling clock.current_time() so often. This reduces
-        // logging noise in simulation.
+        // overhead in simulation.
       }
     }
   }
+
+  tvsc::profile::profiler_stop();
+
+  return 0;
 }
