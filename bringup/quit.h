13e1aeb317cc274289a587bb075af5c0edddc637
James Pack <jgrimm@gmail.com>
2025-03-14 16:21:32 -0400

    Modify the quit task to take a duration. It still defaults to FLAGS_run_duration_sec.

--- bringup/quit.h
+++ bringup/quit.h
@@ -8,9 +8,15 @@ DECLARE_uint64(run_duration_sec);
 
 namespace tvsc::bringup {
 
+using namespace std::chrono_literals;
+
 template <typename ClockType, size_t QUEUE_SIZE>
-tvsc::scheduler::Task<ClockType> quit(scheduler::Scheduler<ClockType, QUEUE_SIZE>& scheduler) {
-  const std::chrono::seconds run_duration{FLAGS_run_duration_sec};
+tvsc::scheduler::Task<ClockType> quit(scheduler::Scheduler<ClockType, QUEUE_SIZE>& scheduler,
+                                      typename ClockType::duration run_duration = 0s) {
+  if (run_duration == 0s) {
+    run_duration = std::chrono::seconds{FLAGS_run_duration_sec};
+  }
+
   // Yield for the duration the script should run.
   co_yield run_duration;
   // And then stop the scheduler.
