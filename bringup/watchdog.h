5340e843774f1073d9c686c21cbb2cedf54c6297
James Pack <jgrimm@gmail.com>
2025-04-01 05:48:25 -0400

    Separate function to run the watchdog into its own header file to make it possible to run in other bringup scripts.

---
+++ bringup/watchdog.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "hal/watchdog/watchdog.h"
+#include "scheduler/task.h"
+
+namespace tvsc::bringup {
+
+template <typename ClockType>
+tvsc::scheduler::Task<ClockType> run_watchdog(
+    tvsc::hal::watchdog::WatchdogPeripheral& watchdog_peripheral) {
+  const auto feed_interval{watchdog_peripheral.reset_interval() / 4};
+
+  // Enable the watchdog here. After this, if the dog isn't fed on time, the board will reset.
+  auto watchdog{watchdog_peripheral.access()};
+  while (true) {
+    co_yield feed_interval;
+    watchdog.feed();
+  }
+}
+
+}  // namespace tvsc::bringup
--- bringup/watchdog_demo.cc
+++ bringup/watchdog_demo.cc
@@ -1,46 +1,15 @@
 #include <chrono>
-#include <cmath>
-#include <cstdint>
-#include <cstring>
-#include <limits>
 
 #include "base/initializer.h"
 #include "bringup/blink.h"
 #include "bringup/quit.h"
+#include "bringup/watchdog.h"
 #include "hal/board/board.h"
 #include "scheduler/scheduler.h"
-#include "scheduler/task.h"
 #include "time/embedded_clock.h"
-
-extern "C" {
-
-__attribute__((section(".status.value"))) volatile uint32_t watchdog_counter{};
-
-}  // extern "C"
-
-namespace tvsc::bringup {
 
 using BoardType = tvsc::hal::board::Board;
 using ClockType = tvsc::time::EmbeddedClock;
-using TaskType = tvsc::scheduler::Task<ClockType>;
-
-using namespace std::chrono_literals;
-
-template <typename Duration = std::chrono::days>
-TaskType run_watchdog(ClockType& clock,
-                      tvsc::hal::watchdog::WatchdogPeripheral& watchdog_peripheral) {
-  const auto feed_interval{watchdog_peripheral.reset_interval() / 4};
-
-  // Enable the watchdog here. After this, if the dog isn't fed on time, the board will reset.
-  auto watchdog{watchdog_peripheral.access()};
-  while (true) {
-    co_yield feed_interval;
-    watchdog.feed();
-    ++watchdog_counter;
-  }
-}
-
-}  // namespace tvsc::bringup
 
 using namespace tvsc::bringup;
 using namespace tvsc::scheduler;
@@ -56,8 +25,8 @@ int main(int argc, char* argv[]) {
   // be able to detect that the watchdog caused a reset.
   clock.sleep(CYCLE_TIME);
 
-  Scheduler<ClockType, 4 /*QUEUE_SIZE*/> scheduler{board.rcc()};
-  scheduler.add_task(run_watchdog(clock, board.iwdg()));
+  Scheduler<ClockType, /* QUEUE_SIZE */ 4> scheduler{board.rcc()};
+  scheduler.add_task(run_watchdog<ClockType>(board.iwdg()));
   scheduler.add_task(
       blink(clock, board.gpio<BoardType::DEBUG_LED_PORT>(), BoardType::DEBUG_LED_PIN));
   scheduler.add_task(quit(scheduler, CYCLE_TIME));
