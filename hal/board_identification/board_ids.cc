3598dd7444e20847d82f8bb05f120df1454a50d4
James Pack <jgrimm@gmail.com>
2025-05-09 12:08:36 -0400

    Filter out default-constructed VoltageDividers with zero resistors from the voltage divider configurations.

--- hal/board_identification/board_ids.cc
+++ hal/board_identification/board_ids.cc
@@ -1,15 +1,29 @@
 #include "hal/board_identification/board_ids.h"
 
+#include <algorithm>
+#include <array>
 #include <cstdint>
 #include <optional>
+#include <vector>
+
+#include "hal/board_identification/voltage_divider_configurations.h"
 
 namespace tvsc::hal::board_identification {
 
-std::array<VoltageDivider<BOARD_ID_ADC_RESOLUTION_BITS, BOARD_ID_RESISTOR_TOLERANCE>, NUM_BOARD_IDS>
+std::vector<VoltageDivider<BOARD_ID_ADC_RESOLUTION_BITS, BOARD_ID_RESISTOR_TOLERANCE>>
 voltage_divider_configurations() {
-  return generate_voltage_divider_configurations<NUM_BOARD_IDS, BOARD_ID_ADC_RESOLUTION_BITS,
-                                                 BOARD_ID_RESISTOR_TOLERANCE>(
-      VOLTAGE_DIVIDER_TOTAL_RESISTANCE);
+  using VoltageDividerType =
+      VoltageDivider<BOARD_ID_ADC_RESOLUTION_BITS, BOARD_ID_RESISTOR_TOLERANCE>;
+
+  static const auto CONFIGURATIONS{
+      generate_voltage_divider_configurations<NUM_BOARD_IDS, BOARD_ID_ADC_RESOLUTION_BITS,
+                                              BOARD_ID_RESISTOR_TOLERANCE>(
+          VOLTAGE_DIVIDER_TOTAL_RESISTANCE)};
+
+  auto end_pos = std::find_if(CONFIGURATIONS.begin(), CONFIGURATIONS.end(),
+                              [](const VoltageDividerType& divider) { return divider.id() == 0; });
+
+  return std::vector<VoltageDividerType>{CONFIGURATIONS.begin(), end_pos};
 }
 
 std::optional<BoardId> determine_board_id(uint16_t adc_measurement) {
--- hal/board_identification/board_ids.h
+++ hal/board_identification/board_ids.h
@@ -1,8 +1,8 @@
 #pragma once
 
-#include <array>
 #include <cstdint>
 #include <optional>
+#include <vector>
 
 #include "hal/board_identification/tolerance.h"
 #include "hal/board_identification/voltage_divider.h"
@@ -10,7 +10,7 @@
 
 namespace tvsc::hal::board_identification {
 
-static constexpr uint8_t NUM_BOARD_IDS{7};
+static constexpr BoardId NUM_BOARD_IDS{7};
 static constexpr uint8_t BOARD_ID_ADC_RESOLUTION_BITS{12};
 static constexpr ResistorTolerance BOARD_ID_RESISTOR_TOLERANCE{ResistorTolerance::E96_1};
 static constexpr float VOLTAGE_DIVIDER_TOTAL_RESISTANCE{25'000.f};
@@ -25,8 +25,7 @@ enum class CanonicalBoardIds : BoardId {
   COMMS_BOARD_2 = 223,
 };
 
-std::array<VoltageDivider<BOARD_ID_ADC_RESOLUTION_BITS, BOARD_ID_RESISTOR_TOLERANCE>,
-           NUM_BOARD_IDS>
+std::vector<VoltageDivider<BOARD_ID_ADC_RESOLUTION_BITS, BOARD_ID_RESISTOR_TOLERANCE>>
 voltage_divider_configurations();
 
 std::optional<BoardId> determine_board_id(uint16_t adc_measurement);
