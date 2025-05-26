9805c201723341d4748f1354bc9bf68453c5dbb0
James Pack <jgrimm@gmail.com>
2025-05-25 21:17:43 -0400

    Create implementation of Serial Wire Debug protocol.

---
+++ serial_wire/serial_wire.cc
@@ -0,0 +1,83 @@
+#include "serial_wire/serial_wire.h"
+
+#include <chrono>
+#include <cstdint>
+
+#include "time/embedded_clock.h"
+
+extern "C" {
+__attribute__((section(".status.value"))) volatile uint32_t swd_target_id{};
+__attribute__((section(".status.value"))) volatile uint32_t trace_fail{};
+__attribute__((section(".status.value"))) uint32_t serial_wire_ack{};
+}
+
+namespace tvsc::serial_wire {
+
+using namespace std::chrono_literals;
+
+[[nodiscard]] bool SerialWire::read(uint8_t command, uint32_t& data) {
+  programmer_.send_no_parity(command, 8);
+  programmer_.receive_no_parity(serial_wire_ack, 3);
+  if (serial_wire_ack != ACK_OK) {
+    return false;
+  }
+
+  if (!programmer_.receive(data, 32)) {
+    return false;
+  }
+
+  programmer_.send_no_parity(0, 8);
+
+  return true;
+}
+
+[[nodiscard]] bool SerialWire::write(uint8_t command, uint32_t data) {
+  programmer_.send_no_parity(command, 8);
+  programmer_.receive_no_parity(serial_wire_ack, 3);
+  if (serial_wire_ack != ACK_OK) {
+    return false;
+  }
+
+  programmer_.send(data, 32);
+  programmer_.send_no_parity(0, 8);
+
+  return true;
+}
+
+/**
+ * Initialize, or re-initialize, the target to accept SWD commands.
+ */
+uint32_t SerialWire::initialize_swd() {
+  using namespace std::chrono_literals;
+
+  // Special value to switch from JTAG to SWD. This value is part of the SWD specification. It can
+  // also be found on page 1577 of the datasheet
+  // https://www.st.com/resource/en/reference_manual/rm0394-stm32l41xxx42xxx43xxx44xxx45xxx46xxx-advanced-armbased-32bit-mcus-stmicroelectronics.pdf.
+  // Note that the datasheet gives the bits in an order such that the MSB needs to be sent first.
+  // Our send routines send the LSB first. This value is reversed from the one in the datasheet.
+  static constexpr uint16_t JTAG_SWD_SWITCHING_SEQUENCE{0xe79e};
+
+  programmer_.initiate_target_board_reset();
+  time::EmbeddedClock::clock().wait(programmer_.RESET_HOLD_TIME);
+  programmer_.conclude_target_board_reset();
+
+  // The SWD spec requires at least 50 clock cycles with SWDIO high to trigger the JTAG/SWD switch
+  // sequence. We send multiple 32-bit values of all ones to accomplish this reset.
+  programmer_.send_no_parity(std::numeric_limits<uint32_t>::max(), 32);
+  programmer_.send_no_parity(std::numeric_limits<uint32_t>::max(), 32);
+
+  // Send the magic value to switch from JTAG to SWD.
+  programmer_.send_no_parity(JTAG_SWD_SWITCHING_SEQUENCE, 8 * sizeof(JTAG_SWD_SWITCHING_SEQUENCE));
+
+  // Again, clock the SWCLK line for at least 50 clock cycles with SWDIO high followed by 4 idle
+  // cycles.
+  programmer_.send_no_parity(std::numeric_limits<uint32_t>::max(), 32);
+  programmer_.send_no_parity(0x0fffffffU, 32);
+
+  // The serial wire specification requires reading the id before the target device is fully
+  // switched from JTAG to SWD.
+  swd_target_id = read_id_code();
+  return swd_target_id;
+}
+
+}  // namespace tvsc::serial_wire
---
+++ serial_wire/serial_wire.h
@@ -0,0 +1,82 @@
+#pragma once
+
+#include <chrono>
+
+#include "bits/bits.h"
+#include "hal/programmer/programmer.h"
+
+namespace tvsc::serial_wire {
+
+using namespace std::chrono_literals;
+
+class SerialWire final {
+ private:
+  tvsc::hal::programmer::Programmer programmer_;
+
+  static constexpr uint8_t BLANK_ACCESS_PORT_READ_COMMAND{0b10000111};
+  static constexpr uint8_t BLANK_ACCESS_PORT_WRITE_COMMAND{0b10000011};
+  static constexpr uint8_t BLANK_DEBUG_PORT_READ_COMMAND{0b10000101};
+  static constexpr uint8_t BLANK_DEBUG_PORT_WRITE_COMMAND{0b10000001};
+  static constexpr uint8_t ACK_OK{0x01};
+
+  static inline void compute_command_parity(uint8_t& command) {
+    uint8_t parity{};
+    for (uint8_t i = 1; i < 5; ++i) {
+      parity += bits::get_bit_field_value<1>(command, i);
+    }
+    bits::modify_bit_field<1, 5>(command, parity);
+  }
+
+  static inline void add_address(uint8_t& command, uint8_t addr) {
+    command |= ((addr & 0x03) << 3);
+  }
+
+  static inline uint8_t create_command(uint8_t command_template, uint8_t addr) {
+    uint8_t command{command_template};
+    add_address(command, addr);
+    compute_command_parity(command);
+    return command;
+  }
+
+  [[nodiscard]] bool read(uint8_t command, uint32_t& data);
+  [[nodiscard]] bool write(uint8_t command, uint32_t data);
+
+ public:
+  SerialWire(tvsc::hal::programmer::ProgrammerPeripheral& programmer_peripheral,
+             std::chrono::nanoseconds clock_period = 100us)
+      : programmer_(programmer_peripheral.access()) {
+    programmer_.set_clock_period(clock_period);
+    initialize_swd();
+  }
+
+  uint32_t read_id_code() {
+    uint32_t id{};
+    if (!swd_dp_read(0x00, id)) {
+      // id = 0;
+    }
+    return id;
+  }
+
+  /**
+   * Initialize, or re-initialize, the target to accept SWD commands.
+   */
+  uint32_t initialize_swd();
+
+  [[nodiscard]] bool swd_ap_read(uint8_t addr, uint32_t& data) {
+    return read(create_command(BLANK_ACCESS_PORT_READ_COMMAND, addr), data);
+  }
+
+  [[nodiscard]] bool swd_ap_write(uint8_t addr, uint32_t data) {
+    return write(create_command(BLANK_ACCESS_PORT_WRITE_COMMAND, addr), data);
+  }
+
+  [[nodiscard]] bool swd_dp_read(uint8_t addr, uint32_t& data) {
+    return read(create_command(BLANK_DEBUG_PORT_READ_COMMAND, addr), data);
+  }
+
+  [[nodiscard]] bool swd_dp_write(uint8_t addr, uint32_t data) {
+    return write(create_command(BLANK_DEBUG_PORT_WRITE_COMMAND, addr), data);
+  }
+};
+
+}  // namespace tvsc::serial_wire
