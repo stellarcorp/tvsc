dc7c11eca640a667f945724b2ebce9106d12ec4d
James Pack <jgrimm@gmail.com>
2025-07-10 11:01:17 -0400

    Refactor SerialWire. Improve error handling. Abstract redundant code.

--- serial_wire/serial_wire.h
+++ serial_wire/serial_wire.h
@@ -18,6 +18,7 @@ class SerialWire final {
   static constexpr uint8_t BLANK_DEBUG_PORT_READ_COMMAND{0b10000101};
   static constexpr uint8_t BLANK_DEBUG_PORT_WRITE_COMMAND{0b10000001};
   static constexpr uint8_t ACK_OK{0x01};
+  static constexpr uint8_t ACK_RETRY{0x02};
 
   static inline void compute_command_parity(uint8_t& command) {
     uint8_t parity{};
@@ -29,6 +30,7 @@ class SerialWire final {
 
   static inline void add_address(uint8_t& command, uint8_t addr) {
     command |= ((addr & 0x03) << 3);
+    // command |= (addr << 1) & (0x03 << 2);
   }
 
   static inline uint8_t create_command(uint8_t command_template, uint8_t addr) {
@@ -38,10 +40,33 @@ class SerialWire final {
     return command;
   }
 
+  [[nodiscard]] bool send_command(uint8_t command);
+
   [[nodiscard]] bool read(uint8_t command, uint32_t& data);
   [[nodiscard]] bool write(uint8_t command, uint32_t data);
 
  public:
+  // Debug port registers.
+
+  // Clear sticky error flags.
+  static constexpr uint8_t DP_ABORT{0x00};
+  // Control/status flags and power requests.
+  static constexpr uint8_t DP_CTRL_STAT{0x04};
+  // Select AP and AP register bank.
+  static constexpr uint8_t DP_SELECT{0x08};
+  // Read buffer (for AP read result).
+  static constexpr uint8_t DP_RDBUFF{0x0C};
+  // [On DPv1] ID register (overlaps with ABORT).
+  static constexpr uint8_t DP_IDCODE{0x00};
+  // [On DPv2+] Replaces IDCODE.
+  static constexpr uint8_t DP_TARGETID{0x00};
+
+  // Access port registers.
+  static constexpr uint8_t AP_CSW{0x00};
+  static constexpr uint8_t AP_TAR{0x04};
+  static constexpr uint8_t AP_DRW{0x0C};
+  static constexpr uint8_t AP_IDR{0xFC};
+
   SerialWire(tvsc::hal::programmer::ProgrammerPeripheral& programmer_peripheral,
              std::chrono::nanoseconds clock_period = 100us)
       : programmer_(programmer_peripheral.access()) {
@@ -59,7 +84,11 @@ class SerialWire final {
   /**
    * Initialize, or re-initialize, the target to accept SWD commands.
    */
-  uint32_t initialize_swd();
+  [[nodiscard]] uint32_t initialize_swd();
+
+  [[nodiscard]] bool clear_dp_state() {
+    return swd_dp_write(DP_ABORT, 0x1E) && swd_dp_write(DP_SELECT, 0x00);
+  }
 
   [[nodiscard]] bool swd_ap_read(uint8_t addr, uint32_t& data) {
     return read(create_command(BLANK_ACCESS_PORT_READ_COMMAND, addr), data);
