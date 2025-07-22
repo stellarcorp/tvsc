5e892e1066aaf4d325cf841a1e19e000f76082c6
James Pack <jgrimm@gmail.com>
2025-07-22 10:37:17 -0400

    Add symbol to linker script to specify flash page size. Update firmware_size to round up to the nearest page boundary. Add meta symbols to specify flash page size and number of pages used by firmware.

--- meta/firmware.cc
+++ meta/firmware.cc
@@ -8,9 +8,16 @@ namespace tvsc::meta {
 extern "C" {
 extern const char FIRMWARE_BEGIN;
 extern const char FIRMWARE_END;
+extern const char FLASH_PAGE_SIZE_BYTES;
 }
+const size_t flash_page_size_bytes{reinterpret_cast<size_t>(&FLASH_PAGE_SIZE_BYTES)};
 
 const uint32_t* const firmware_start{reinterpret_cast<const uint32_t*>(&FIRMWARE_BEGIN)};
-const size_t firmware_size{static_cast<size_t>(&FIRMWARE_END - &FIRMWARE_BEGIN)};
+
+const size_t firmware_num_pages{
+    (static_cast<size_t>(&FIRMWARE_END - &FIRMWARE_BEGIN) + flash_page_size_bytes - 1) /
+    flash_page_size_bytes};
+
+const size_t firmware_size{firmware_num_pages * flash_page_size_bytes};
 
 }  // namespace tvsc::meta
--- meta/firmware.h
+++ meta/firmware.h
@@ -5,6 +5,8 @@
 namespace tvsc::meta {
 
 extern const uint32_t* const firmware_start;
-extern const size_t firmware_size;
+extern const size_t firmware_size_bytes;
+extern const size_t firmware_size_pages;
+extern const size_t flash_page_size_bytes;
 
 }  // namespace tvsc::meta
