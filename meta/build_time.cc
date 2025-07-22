e661bd23b576f739d551cf9303e951a51d84755f
James Pack <jgrimm@gmail.com>
2025-07-22 14:43:04 -0400

    Use bazel to generate build timestamp, rather than rely on bash script in a genrule.

---
+++ meta/build_time.cc
@@ -0,0 +1,16 @@
+// Auto-generated file — do not edit manually
+
+#include "meta/build_time.h"
+
+#include <cstdint>
+
+namespace tvsc::meta {
+
+__attribute__((section(".build_time"))) const BuildTime BUILD_TIME{
+    BUILD_TIMESTAMP,  // Symbol defined by Bazel during build.
+};
+
+__attribute__((section(".build_time")))
+const uint32_t BUILD_TIME_ADDR{reinterpret_cast<uint32_t>(&BUILD_TIME)};
+
+}  // namespace tvsc::meta
--- meta/build_time.h
+++ meta/build_time.h
@@ -6,9 +6,6 @@ namespace tvsc::meta {
 
 struct BuildTime final {
   uint32_t timestamp;
-  // The build time string below uses ISO 8601 with precision to the second. Example:
-  // "2025-07-07T14:58:03Z". This string will require 20 bytes plus one byte of null-termination.
-  char build_time_str[21];
 };
 
 extern const BuildTime BUILD_TIME;
