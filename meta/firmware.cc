9a1499c687797f6a8f868179d498c9b944613dc6
James Pack <jgrimm@gmail.com>
2025-07-22 14:44:38 -0400

    Round the firmware size up to the nearest number of flash pages. Expose the number of flash pages required by the firmware as well as the size of a flash page.

--- meta/firmware.cc
+++ meta/firmware.cc
@@ -1,23 +1,23 @@
 #include "meta/firmware.h"
 
 #include <cstddef>
-#include <string_view>
+#include <cstdint>
+
+#include "meta/flash.h"
 
 namespace tvsc::meta {
 
 extern "C" {
 extern const char FIRMWARE_BEGIN;
 extern const char FIRMWARE_END;
-extern const char FLASH_PAGE_SIZE_BYTES;
 }
-const size_t flash_page_size_bytes{reinterpret_cast<size_t>(&FLASH_PAGE_SIZE_BYTES)};
 
 const uint32_t* const firmware_start{reinterpret_cast<const uint32_t*>(&FIRMWARE_BEGIN)};
 
-const size_t firmware_num_pages{
-    (static_cast<size_t>(&FIRMWARE_END - &FIRMWARE_BEGIN) + flash_page_size_bytes - 1) /
-    flash_page_size_bytes};
+const size_t firmware_size_pages{
+    (static_cast<size_t>(&FIRMWARE_END - &FIRMWARE_BEGIN) + FLASH_PAGE_SIZE_BYTES - 1) /
+    FLASH_PAGE_SIZE_BYTES};
 
-const size_t firmware_size{firmware_num_pages * flash_page_size_bytes};
+const size_t firmware_size{firmware_size_pages * FLASH_PAGE_SIZE_BYTES};
 
 }  // namespace tvsc::meta
--- meta/firmware.h
+++ meta/firmware.h
@@ -1,12 +1,12 @@
 #pragma once
 
-#include <string_view>
+#include <cstddef>
+#include <cstdint>
 
 namespace tvsc::meta {
 
 extern const uint32_t* const firmware_start;
 extern const size_t firmware_size_bytes;
 extern const size_t firmware_size_pages;
-extern const size_t flash_page_size_bytes;
 
 }  // namespace tvsc::meta
