7fa2bd82f7f75700f6de9e02fb3252230862e182
James Pack <jgrimm@gmail.com>
2025-07-23 11:25:28 -0400

    Extract the flashing functionality from flash_target.cc into a header file. Create another bringup script, satellite, to act as an integration point for the tasks needed for the LEO satellite launch.

--- bringup/satellite.cc
+++ bringup/satellite.cc
@@ -1,6 +1,6 @@
-#include "bringup/flash_target.h"
-
 #include "base/initializer.h"
+#include "bringup/blink.h"
+#include "bringup/flash_target.h"
 #include "hal/board/board.h"
 #include "scheduler/scheduler.h"
 #include "time/embedded_clock.h"
@@ -11,13 +11,17 @@ using ClockType = tvsc::time::EmbeddedClock;
 using namespace tvsc::bringup;
 using namespace tvsc::scheduler;
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
   tvsc::initialize(&argc, &argv);
 
-  BoardType &board{BoardType::board()};
+  auto& board{BoardType::board()};
+  auto& clock{ClockType::clock()};
 
-  Scheduler<ClockType, 1 /*QUEUE_SIZE*/> scheduler{board.rcc()};
+  static constexpr size_t QUEUE_SIZE{4};
+  Scheduler<ClockType, QUEUE_SIZE> scheduler{board.rcc()};
   scheduler.add_task(flash_target<ClockType>(
       board.programmer(), board.gpio<BoardType::DEBUG_LED_PORT>(), BoardType::DEBUG_LED_PIN));
+  scheduler.add_task(
+      blink(clock, board.gpio<BoardType::DEBUG_LED_PORT>(), BoardType::DEBUG_LED_PIN));
   scheduler.start();
 }
