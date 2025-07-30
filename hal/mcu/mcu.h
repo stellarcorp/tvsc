35c88d3aada8d2b51c50e72e4a4930317f698aa5
James Pack <jgrimm@gmail.com>
2025-07-30 12:57:42 -0400

    Add MCU method to compute a hashed version of the MCU's id.

--- hal/mcu/mcu.h
+++ hal/mcu/mcu.h
@@ -3,6 +3,9 @@
 #include <array>
 #include <cstdint>
 
+#include "hash/hash_combine.h"
+#include "hash/integer_hash.h"
+
 namespace tvsc::hal::mcu {
 
 using McuId = std::array<uint32_t, 3>;
@@ -12,6 +15,15 @@ class Mcu {
   virtual ~Mcu() = default;
 
   virtual void read_id(McuId& id) = 0;
+
+  uint8_t hashed_id() {
+    McuId id;
+    read_id(id);
+    const auto hashed_id{hash::hash_combine(0, hash::integer_hash(id[0]), hash::integer_hash(id[1]),
+                                            hash::integer_hash(id[2]))};
+
+    return static_cast<uint8_t>(hashed_id);
+  }
 };
 
 }  // namespace tvsc::hal::mcu
