398f7f5ff9bb8d909a211e92c9768c8d9ee3c6d2
James Pack <jgrimm@gmail.com>
2025-07-31 14:10:02 -0400

    Add task to receive CAN bus transmissions. Currently, it just blinks on valid reception; need to add a queue.

---
+++ bringup/can_rx.h
@@ -0,0 +1,59 @@
+#pragma once
+
+#include <chrono>
+
+#include "hal/can_bus/can_bus.h"
+#include "message/message.h"
+#include "scheduler/task.h"
+
+namespace tvsc::bringup {
+
+template <typename ClockType>
+tvsc::scheduler::Task<ClockType> can_bus_receive(
+    tvsc::hal::can_bus::CanBusPeripheral& can_peripheral,
+    tvsc::hal::gpio::GpioPeripheral& gpio_peripheral, tvsc::hal::gpio::Pin pin) {
+  using namespace std::chrono_literals;
+  using namespace tvsc::hal::can_bus;
+
+  {
+    // Turn on the LED GPIO.
+    tvsc::hal::gpio::Gpio led{gpio_peripheral.access()};
+
+    led.set_pin_mode(pin, tvsc::hal::gpio::PinMode::OUTPUT_PUSH_PULL);
+
+    for (int i = 0; i < 5; ++i) {
+      led.write_pin(pin, 1);
+      co_yield 25ms;
+      led.write_pin(pin, 0);
+      co_yield 25ms;
+    }
+  }
+
+  // Turn on clock for the CAN bus.
+  auto can{can_peripheral.access()};
+  while (true) {
+    while (can.available_message_count(RxFifo::FIFO_ZERO) > 0) {
+      // Turn on the LED GPIO.
+      tvsc::hal::gpio::Gpio led{gpio_peripheral.access()};
+
+      led.set_pin_mode(pin, tvsc::hal::gpio::PinMode::OUTPUT_PUSH_PULL);
+
+      tvsc::message::CanBusMessage message{};
+      if (can.receive(RxFifo::FIFO_ZERO, message)) {
+        led.write_pin(pin, 1);
+        co_yield 100ms;
+        led.write_pin(pin, 0);
+      } else {
+        for (int i = 0; i < 5; ++i) {
+          led.write_pin(pin, 1);
+          co_yield 25ms;
+          led.write_pin(pin, 0);
+          co_yield 25ms;
+        }
+      }
+    }
+    co_yield 50ms;
+  }
+}
+
+}  // namespace tvsc::bringup
