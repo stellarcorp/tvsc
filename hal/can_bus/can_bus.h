deb66229205cdcfadf6c37fb9456906a993323b2
James Pack <jgrimm@gmail.com>
2025-07-31 13:21:23 -0400

    Add CAN bus methods to send Messages, rather than just raw ids and payloads.

--- hal/can_bus/can_bus.h
+++ hal/can_bus/can_bus.h
@@ -5,6 +5,7 @@
 #include <string>
 
 #include "hal/peripheral.h"
+#include "message/message.h"
 
 namespace tvsc::hal::can_bus {
 
@@ -56,11 +57,15 @@ class CanBus final : public Functional<CanBusPeripheral, CanBus> {
     return peripheral_->available_message_count(fifo);
   }
 
-  bool receive(RxFifo fifo, uint32_t& identifier, std::array<uint8_t, 8>& data) {
+  bool receive_raw(RxFifo fifo, uint32_t& identifier, std::array<uint8_t, 8>& data) {
     return peripheral_->receive(fifo, identifier, data);
   }
 
-  bool transmit(uint32_t identifier, const std::array<uint8_t, 8>& data) {
+  bool receive(RxFifo fifo, message::CanBusMessage& message) {
+    return peripheral_->receive(fifo, message.identifier(), message.payload());
+  }
+
+  bool transmit_raw(uint32_t identifier, const std::array<uint8_t, 8>& data) {
     return peripheral_->transmit(identifier, data);
   }
 
@@ -69,6 +74,10 @@ class CanBus final : public Functional<CanBusPeripheral, CanBus> {
     const std::size_t copy_length{std::min(data.size(), str.size())};
     std::copy(str.begin(), str.begin() + copy_length, data.begin());
     return peripheral_->transmit(identifier, data);
+  }
+
+  bool transmit(const message::CanBusMessage& message) {
+    return peripheral_->transmit(message.identifier(), message.payload());
   }
 
   uint32_t error_code() const { return peripheral_->error_code(); }
